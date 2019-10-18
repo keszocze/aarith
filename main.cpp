5538d20b540a28f5a3a6b4ede71dcef4fd3bbe70
Oliver Keszöcze <oliver.keszoecze@fau.de>
2019-10-18 15:16:42 +0200

    initial 'working' commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include <integer.hpp>
+
+int main() {
+
+
+    integer<7> foo;
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
---
+++ src/types/integer.hpp
@@ -0,0 +1,14 @@
+//
+// Created by keszocze on 18.10.19.
+//
+
+#ifndef AARITH_INTEGER_HPP
+#define AARITH_INTEGER_HPP
+
+template<size_t W>
+class integer
+{
+
+};
+
+#endif //AARITH_INTEGER_HPP
