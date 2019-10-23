1adb5416e9113206ee1904cc788db74fbbcdb126
Michael Witterauf <witterauf@cs.fau.de>
2019-10-23 16:08:55 +0200

    Modernize CMakeLists

--- src/types/integer.hpp
+++ src/types/integer.hpp
@@ -1,44 +1,33 @@
-//
-// Created by keszocze on 18.10.19.
-//
-
 #pragma once
 
+#include <array>
 #include <cstdint>
 #include <type_traits>
-#include <array>
 
-template <class T>
-constexpr size_t size_in__words(const size_t w) {
-    const u_int word_size = sizeof(T)*8;
-    return (w / word_size)  + (w % word_size ? 1  : 0);
+template <class T> constexpr size_t size_in_words(const size_t w)
+{
+    const auto word_size = sizeof(T) * 8;
+    return (w / word_size) + (w % word_size ? 1 : 0);
 }
 
-
-template <size_t Width> class uinteger {
+template <size_t Width> class uinteger
+{
 private:
+    using integer_type = uint64_t;
+    static_assert(Width > 0, "Width must be at least 1 (bit)");
+    static constexpr size_t array_size = size_in_words<integer_type>(Width);
 
-    static_assert(Width > 0, "Need at least 1 bit");
-
-    static constexpr size_t  array_size = size_in__words<uint64_t>(Width);
-
-    std::array<uint64_t, array_size> value;
+    std::array<integer_type, array_size> value;
 
 public:
-
-    template <class T>
-    uinteger(T n) {
-
-
+    template <class T> uinteger(T n)
+    {
         static_assert(std::is_integral<T>::value, "Only integral values are supported");
         static_assert(!std::is_signed<T>::value, "Only unsigned numbers are supported");
-        static_assert(sizeof(T)*8<=64,"Only up to 64 bit integers are supported");
-        static_assert(sizeof(T)*8 <= Width, "Data type can not fit provided number");
+        static_assert(sizeof(T) * 8 <= sizeof(integer_type) * 8,
+                      "Only up to 64 bit integers are supported");
+        static_assert(sizeof(T) * 8 <= Width, "Data type can not fit provided number");
 
-        value[0]=n;
-
+        value[0] = n;
     }
-
 };
-
-
--- tests/uint-test.cpp
+++ tests/uint-test.cpp
@@ -1,11 +1,11 @@
+#include <catch2/catch.hpp>
+#include <integer.hpp>
 #include <iostream>
 
-#include <integer.hpp>
-
-#include <catch2/catch.hpp>
-
-
-
-TEST_CASE("reject signed numbers") {
+TEST_CASE("reject signed numbers")
+{
     CHECK(0 == 1);
 }
+
+// for static_assert tests:
+// https://stackoverflow.com/questions/30155619/expected-build-failure-tests-in-cmake
