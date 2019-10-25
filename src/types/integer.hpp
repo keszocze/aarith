db80f6719a419c8ac82604f4e4c02c7e3fb63173
Michael Witterauf <witterauf@cs.fau.de>
2019-10-25 13:40:34 +0200

    Implement rudimentary exact addition

--- src/types/integer.hpp
+++ src/types/integer.hpp
@@ -2,7 +2,10 @@
 
 #include <array>
 #include <cstdint>
+#include <iostream>
 #include <type_traits>
+
+namespace aarith {
 
 template <class T> constexpr size_t size_in_words(const size_t w)
 {
@@ -12,22 +15,104 @@ template <class T> constexpr size_t size_in_words(const size_t w)
 
 template <size_t Width> class uinteger
 {
-private:
-    using integer_type = uint64_t;
-    static_assert(Width > 0, "Width must be at least 1 (bit)");
-    static constexpr size_t array_size = size_in_words<integer_type>(Width);
-
-    std::array<integer_type, array_size> value;
-
 public:
-    template <class T> uinteger(T n)
+    using word_type = uint64_t;
+    using bit_type = word_type;
+    static_assert(Width > 0, "Width must be at least 1 (bit)");
+
+    uinteger() = default;
+
+    template <class T>
+    explicit uinteger(T n)
+        : words{0}
     {
         static_assert(std::is_integral<T>::value, "Only integral values are supported");
         static_assert(!std::is_signed<T>::value, "Only unsigned numbers are supported");
-        static_assert(sizeof(T) * 8 <= sizeof(integer_type) * 8,
+        static_assert(sizeof(T) * 8 <= sizeof(word_type) * 8,
                       "Only up to 64 bit integers are supported");
         static_assert(sizeof(T) * 8 <= Width, "Data type can not fit provided number");
 
-        value[0] = n;
+        words[0] = n;
     }
+
+    static constexpr auto word_width() -> size_t
+    {
+        return sizeof(word_type) * 8;
+    }
+
+    static constexpr auto word_count() -> size_t
+    {
+        return size_in_words<word_type>(Width);
+    }
+
+    static constexpr auto word_mask(size_t index) -> word_type
+    {
+        auto const last_mask = (static_cast<word_type>(1) << (Width % word_width())) - 1;
+        auto const other_masks = static_cast<word_type>(-1);
+        return index == word_count() - 1 ? last_mask : other_masks;
+    };
+
+    auto word(size_t index) const -> word_type
+    {
+        return words[index];
+    }
+
+    void set_word(size_t index, word_type value)
+    {
+        words[index] = value & word_mask(index);
+    }
+
+    auto bit(size_t index) const -> bit_type
+    {
+        auto const the_word = word(index / word_width());
+        auto const masked_bit = the_word & (static_cast<word_type>(1) << (index % word_width()));
+        return static_cast<bit_type>(masked_bit > 0 ? 1 : 0);
+    }
+
+private:
+    std::array<word_type, word_count()> words{0};
 };
+
+template <class Type> class is_integral
+{
+public:
+    static constexpr bool value = false;
+};
+
+template <size_t Width> class is_integral<uinteger<Width>>
+{
+public:
+    static constexpr bool value = true;
+};
+
+template <size_t DestinationWidth, size_t SourceWidth>
+auto width_cast(const uinteger<SourceWidth>& source) -> uinteger<DestinationWidth>
+{
+    uinteger<DestinationWidth> destination;
+    if constexpr (DestinationWidth >= SourceWidth)
+    {
+        for (auto i = 0U; i < SourceWidth; ++i)
+        {
+            destination.set_word(i, source.word(i));
+        }
+    }
+    else
+    {
+        for (auto i = 0U; i < DestinationWidth; ++i)
+        {
+            destination.set_word(i, source.word(i));
+        }
+    }
+}
+
+template <size_t Width>
+auto operator<<(const uinteger<Width>& value, std::ostream& out) -> std::ostream&
+{
+    for (auto i = Width; i > 0; --i)
+    {
+        std::cout << value.bit(i - 1);
+    }
+    return out;
+}
+
+} // namespace aarith
--- tests/uint-test.cpp
+++ tests/uint-test.cpp
@@ -1,10 +1,44 @@
 #include <catch.hpp>
-#include <integer.hpp>
 #include <iostream>
+#include <operations/exact_operations.hpp>
+#include <types/integer.hpp>
 
-TEST_CASE("reject signed numbers")
+using namespace aarith;
+
+SCENARIO("Exact sum of unsigned integers", "[uinteger]")
 {
-    CHECK(0 == 1);
+    GIVEN("Two uintegers with width < word_width")
+    {
+        static constexpr size_t TestWidth = 16;
+        static_assert(uinteger<TestWidth>::word_count() == 1);
+
+        WHEN("The result still fits the width")
+        {
+            static constexpr uint16_t number_a = 32;
+            static constexpr uint16_t number_b = 16;
+            uinteger<TestWidth> a{number_a};
+            uinteger<TestWidth> b{number_b};
+            auto result = exact_uint_add(a, b);
+
+            THEN("It should be the correct sum")
+            {
+                REQUIRE(result.word(0) == number_a + number_b);
+            }
+        }
+        WHEN("The result does not fit the width")
+        {
+            static constexpr uint16_t number_a = 65535;
+            static constexpr uint16_t number_b = 1;
+            uinteger<TestWidth> a{number_a};
+            uinteger<TestWidth> b{number_b};
+            auto result = exact_uint_add(a, b);
+
+            THEN("It should be the masked to fit")
+            {
+                REQUIRE(result.word(0) == 0);
+            }
+        }
+    }
 }
 
 // for static_assert tests:
