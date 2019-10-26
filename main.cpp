cddb1a6bbecac85ffc18ca572998a5201ffae654
Oliver Keszöcze <oliver.keszoecze@fau.de>
2019-10-26 18:01:46 +0200

    add test cases for word_mask computation and subtraction

--- main.cpp
+++ main.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
+#include <bitset>
+#include <aarith/types/integer.hpp>
+#include <aarith/operations/exact_operations.hpp>
 
-#include <integer.hpp>
 
 int main() {
 
+    using namespace aarith;
 
-    uinteger<32> foo{0u};
-    std::cout << "Hello, World!" << std::endl;
+    static constexpr uint8_t number_a1 = 20;
+    static constexpr uint8_t number_b1 = 100;
+
+    static constexpr uint8_t num_res = number_a1 - number_b1;
+
+    static constexpr size_t TestWidth1 = 8;
+
+    const uinteger<TestWidth1> a1{number_a1};
+    const uinteger<TestWidth1> b1{number_b1};
+
+    auto const result = exact_uint_sub(a1, b1);
+
+    std::cout << result << "\t" << result.word_mask(0) << "\t" << uinteger<64>{result.word(0)} << "\t" << num_res << "\n";
+
+
     return 0;
 }
