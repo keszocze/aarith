290ed97c2ea3314fb87970468d8d4e75e4455f67
Oliver Keszöcze <oliver.keszoecze@fau.de>
2019-11-08 16:24:08 +0100

    stah

--- src/aarith/operations/exact_operations.hpp
+++ src/aarith/operations/exact_operations.hpp
@@ -44,7 +44,7 @@ template <class UInteger> auto exact_uint_add(const UInteger& a, const UInteger&
         static_assert(is_unsigned<UInteger>::value);
 
         UInteger product;
-        uint64_t carry{0};
+        typename UInteger::word_type carry{0};
 
         for (auto i = 0U; i < a.word_count(); ++i)
         {
@@ -52,15 +52,25 @@ template <class UInteger> auto exact_uint_add(const UInteger& a, const UInteger&
             const auto [a_upper,a_lower] = split(a.word(i));
             const auto [b_upper,b_lower] = split(b.word(i));
 
-            const uint64_t pp1 = a_lower * b_lower;
-            const uint64_t pp2 = a_lower * b_upper;
-            const uint64_t pp3 = a_upper * b_lower;
-            const uint64_t pp4 = a_upper * b_upper;
+            const typename UInteger::word_type pp1 = a_lower * b_lower;
+            const typename UInteger::word_type pp2 = a_lower * b_upper;
+            const typename UInteger::word_type pp3 = a_upper * b_lower;
+            const typename UInteger::word_type pp4 = a_upper * b_upper;
 
-            
 
-            auto const partial_sum = a.word(i) + b.word(i) + carry;
+            auto partial_sum = pp1 + carry;
+
+            carry = (partial_sum < pp1 || partial_sum < carry) ? 1 : 0;
+
+            const auto [carry_pp1,lower_pp1] = split(pp1);
+
+
+            partial_sum = pp2+ pp3  + carry_pp1;
             carry = (partial_sum < a.word(i) || partial_sum < b.word(i)) ? 1 : 0;
+
+
+
+
             product.set_word(i, partial_sum);
         }
         return product;
