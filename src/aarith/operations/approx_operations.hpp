0f3b40345d5a19cae7cfef27655c512c38a9171b
Marcel Brand <marcel.brand@fau.de>
2019-11-11 18:36:16 +0100

    Adds anytime bitmasking mul for uint with simple tests

--- src/aarith/operations/approx_operations.hpp
+++ src/aarith/operations/approx_operations.hpp
@@ -2,28 +2,31 @@
 
 #include <cstdint>
 #include <iostream>
+#include "aarith/types/integer.hpp"
 
 namespace aarith {
 
-template <class UInteger> 
-auto generate_bitmask(const uint32_t bits) 
--> UInteger
+template <class UInteger> auto generate_bitmask(const uint32_t bits) -> UInteger
 {
     UInteger mask;
 
-    constexpr typename UInteger::word_type full_mask = static_cast<typename UInteger::word_type>(-1);
-    const auto full_mask_words = bits/static_cast<uint32_t>(UInteger::word_width());
-    const auto remaining_bits = bits - full_mask_words * static_cast<uint32_t>(UInteger::word_width());
+    constexpr typename UInteger::word_type full_mask =
+        static_cast<typename UInteger::word_type>(-1);
+    const auto full_mask_words = bits / static_cast<uint32_t>(UInteger::word_width());
+    const auto remaining_bits =
+        bits - full_mask_words * static_cast<uint32_t>(UInteger::word_width());
 
-    const typename UInteger::word_type last_word_mask = (static_cast<typename UInteger::word_type>(1) << bits) - 1;
+    const typename UInteger::word_type last_word_mask =
+        (static_cast<typename UInteger::word_type>(1) << bits) - 1;
 
     auto counter = 0U;
-    while(counter < full_mask_words){
+    while (counter < full_mask_words)
+    {
         mask.set_word(counter, full_mask);
         ++counter;
     }
-    
-    if(remaining_bits > 0)
+
+    if (remaining_bits > 0)
     {
         mask.set_word(counter, last_word_mask);
     }
@@ -31,22 +34,45 @@ auto generate_bitmask(const uint32_t bits)
     return mask;
 }
 
-template <class UInteger> 
-auto approx_uint_bitmasking_add(const UInteger& opd1, const UInteger& opd2, const uint32_t bits) 
--> UInteger
+template <class UInteger>
+auto approx_uint_bitmasking_add(const UInteger& opd1, const UInteger& opd2, const uint32_t bits)
+    -> UInteger
 {
     UInteger sum, mask;
 
-    mask = generate_bitmask<UInteger>(UInteger::width()-bits);
+    mask = generate_bitmask<UInteger>(UInteger::width() - bits);
     mask = ~mask;
 
     UInteger opd1_masked = opd1 & mask;
     UInteger opd2_masked = opd2 & mask;
-    
+
     sum = exact_uint_add(opd1_masked, opd2_masked);
 
     return sum;
 }
 
+template <size_t width>
+auto approx_uint_bitmasking_mul(const uinteger<width>& opd1, const uinteger<width>& opd2, const uint32_t bits)
+    -> uinteger<2*width>
+{
+    constexpr auto product_width = 2 * width;
+    uinteger<product_width> product, mask;
+
+    mask = generate_bitmask<uinteger<product_width>>(product_width - bits);
+    mask = ~mask;
+
+    uinteger<product_width> opd2_extended = width_cast<product_width, width>(opd2);
+
+    for (auto counter = 0U; counter < width; ++counter)
+    {
+        auto opd2_masked = opd2_extended & mask;
+        product = ((opd1.bit(counter) == 0) ? (product) : (exact_uint_add(product, opd2_masked)));
+        opd2_extended = opd2_extended << 1;
+
+        std::cout << "iteration " << counter << ": product = " << product.word(0) << std::endl;
+    }
+
+    return product;
+}
 
 } // namespace aarith
--- tests/uint-anytime-test.cpp
+++ tests/uint-anytime-test.cpp
@@ -1,5 +1,5 @@
-#include "aarith/operations/exact_operations.hpp"
 #include "aarith/operations/approx_operations.hpp"
+#include "aarith/operations/exact_operations.hpp"
 #include "aarith/types/integer.hpp"
 #include <aarith/operations/comparisons.hpp>
 #include <catch.hpp>
@@ -40,7 +40,6 @@ SCENARIO("Generate a bitmask for a certain number of bits", "[uinteger][utlitiy]
                 REQUIRE(result.word(1) == result_ref_1);
                 REQUIRE(result.word(2) == result_ref_2);
             }
-            
         }
         WHEN("The bitmask is as long as the complete uinteger and fills all words completely")
         {
@@ -58,7 +57,6 @@ SCENARIO("Generate a bitmask for a certain number of bits", "[uinteger][utlitiy]
                 REQUIRE(result.word(1) == result_ref_1);
                 REQUIRE(result.word(2) == result_ref_2);
             }
-            
         }
     }
 }
@@ -114,6 +112,68 @@ SCENARIO("Approximate sum of unsigned integers with anytime bitmasking", "[uinte
     }
 }
 
+SCENARIO("Approximate product of unsigned integers with anytime bitmasking",
+         "[uinteger][arithmetic]")
+{
+    GIVEN("Two uintegers with width < word_width")
+    {
+        static constexpr size_t TestWidth = 8;
+        static_assert(uinteger<TestWidth>::word_count() == 1);
+        static constexpr uint8_t number_a = 170U;
+        static constexpr uint8_t number_b = 255U;
+        const uinteger<TestWidth> a{number_a};
+        const uinteger<TestWidth> b{number_b};
+
+        WHEN("One bit is calculated")
+        {
+            auto const result = approx_uint_bitmasking_mul(a, b, 1U);
+
+            REQUIRE(result.word(0) == 0);
+        }
+        WHEN("Two bits are calculated")
+        {
+            auto const result = approx_uint_bitmasking_mul(a, b, 2U);
+            auto const result_2bits = 16384U;
+
+            REQUIRE(result.word(0) == result_2bits);
+        }
+        WHEN("Two bits are calculated and a and b are switched")
+        {
+            auto const result = approx_uint_bitmasking_mul(b, a, 2U);
+            auto const result_2bits = 16384U;
+
+            REQUIRE(result.word(0) == result_2bits);
+        }
+        WHEN("Eight bits are calculated")
+        {
+            auto const result = approx_uint_bitmasking_mul(a, b, 8U);
+            auto const result_8bits = 42496U;
+
+            REQUIRE(result.word(0) == result_8bits);
+        }
+        WHEN("Eight bits are calculated and a and b are switched")
+        {
+            auto const result = approx_uint_bitmasking_mul(b, a, 8U);
+            auto const result_8bits = 42496U;
+
+            REQUIRE(result.word(0) == result_8bits);
+        }
+        WHEN("All bits are calculated")
+        {
+            auto const result = approx_uint_bitmasking_mul(a, b, 16U);
+            auto const result_16bits = 43350U;
+
+            REQUIRE(result.word(0) == result_16bits);
+        }
+        WHEN("All bits are calculated and a and b are switched")
+        {
+            auto const result = approx_uint_bitmasking_mul(b, a, 16U);
+            auto const result_16bits = 43350U;
+
+            REQUIRE(result.word(0) == result_16bits);
+        }
+    }
+}
 
 // for static_assert tests:
 // https://stackoverflow.com/questions/30155619/expected-build-failure-tests-in-cmake
