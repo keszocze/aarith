d19672784724b51d1c8416d2a04fe4e3d905491d
Oliver Keszöcze <oliver.keszoecze@fau.de>
2019-11-26 14:00:21 +0100

    Make it compile nicely.

--- main.cpp
+++ main.cpp
@@ -8,22 +8,6 @@ int main() {
 
     using namespace aarith;
 
-    static constexpr uint8_t number_a1 = 20;
-    static constexpr uint8_t number_b1 = 100;
-
-    static constexpr uint8_t num_res = number_a1 - number_b1;
-
-    static constexpr size_t TestWidth1 = 8;
-
-    const uinteger<TestWidth1> a1{number_a1};
-    const uinteger<TestWidth1> b1{number_b1};
-
-    auto const result = exact_uint_sub(a1, b1);
-    auto const blub = uinteger<192>::from_words(1, 2, 3);
-
-    std::cout << blub;
-    std::cout << result << "\t" << result.word_mask(0) << "\t" << uinteger<64>{result.word(0)} << "\t" << num_res << "\n";
-
 
     return 0;
 }
