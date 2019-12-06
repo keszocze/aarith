d9af557da9393ac22f282171d12c96dfa4b78b99
Oliver Keszöcze <oliver.keszoecze@fau.de>
2019-12-06 13:46:04 +0100

    Try to add limit specializations to uinteger
    For some reason, this fails.

--- src/aarith/types/traits.hpp
+++ src/aarith/types/traits.hpp
@@ -1,5 +1,9 @@
 #pragma once
 
+#include "aarith/types/integer.hpp"
+
+#include <limits>
+
 namespace aarith {
 
 template <class Type> class is_integral
@@ -15,3 +19,13 @@ public:
 };
 
 } // namespace aarith
+
+// We are only allowed to extend std with  specializations
+// https://en.cppreference.com/w/cpp/language/extending_std
+template <size_t W>
+class std::numeric_limits<aarith::uinteger<W>>
+{
+    static constexpr bool is_exact = true;
+    static constexpr bool is_bounded = true;
+    static constexpr bool is_signed = false;
+}
