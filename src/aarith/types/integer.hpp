a68cd46b9a53a59cff8fd48fb85a6ec01c378f87
Michael Witterauf <witterauf@cs.fau.de>
2019-12-06 14:14:15 +0100

    Remove cyclic dependency between headers

--- src/aarith/types/integer.hpp
+++ src/aarith/types/integer.hpp
@@ -441,3 +441,13 @@ template <size_t Width> auto count_leading_zeroes(const uinteger<Width>& value) 
 }
 
 } // namespace aarith
+
+// We are only allowed to extend std with specializations
+// https://en.cppreference.com/w/cpp/language/extending_std
+template <size_t W> class std::numeric_limits<aarith::uinteger<W>>
+{
+public:
+    static constexpr bool is_exact = true;
+    static constexpr bool is_bounded = true;
+    static constexpr bool is_signed = false;
+};
