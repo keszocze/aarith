91c377afcf71c67932d44b0b90113616142c22d8
Oliver Keszöcze <oliver.keszoecze@fau.de>
2019-12-06 13:17:54 +0100

    Extract the random number generation even further.
    Fixes #19

--- tests/uint-random-test.cpp
+++ tests/uint-random-test.cpp
@@ -1,47 +1,14 @@
 #include "aarith/operations/exact_operations.hpp"
 #include "aarith/types/integer.hpp"
-#include "aarith/types/random.hpp"
+
 #include "aarith/utilities/string_utils.hpp"
+#include "gen_uinteger.hpp"
+
 #include <catch.hpp>
 #include <iostream>
 #include <mpir.h>
 
 using namespace aarith;
-
-template <size_t BitWidth>
-class UIntegerGenerator : public Catch::Generators::IGenerator<uinteger<BitWidth>>
-{
-public:
-    explicit UIntegerGenerator(size_t, size_t)
-        : rng{std::random_device{}()}
-    {
-    }
-
-    auto get() const -> const uinteger<BitWidth>& override
-    {
-        return current_number;
-    }
-
-    bool next() override
-    {
-        current_number = random_number(rng);
-        return true;
-    }
-
-private:
-    std::minstd_rand rng;
-    uinteger<BitWidth> current_number;
-    uniform_uinteger_distribution<BitWidth> random_number;
-};
-
-template <size_t BitWidth>
-auto random_uinteger(size_t min_length, size_t max_length)
-    -> Catch::Generators::GeneratorWrapper<uinteger<BitWidth>>
-{
-    return Catch::Generators::GeneratorWrapper<uinteger<BitWidth>>(
-        std::unique_ptr<Catch::Generators::IGenerator<uinteger<BitWidth>>>(
-            new UIntegerGenerator<BitWidth>(min_length, max_length)));
-}
 
 template <size_t BitWidth>
 bool is_uint_add_correct(const uinteger<BitWidth>& lhs, const uinteger<BitWidth>& rhs,
