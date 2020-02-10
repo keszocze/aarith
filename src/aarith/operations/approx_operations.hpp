ca9c967035f705732842d32950f3e7bdf852aba7
Oliver Keszöcze <oliver.keszoecze@fau.de>
2020-02-10 13:52:52 +0100

    Add FAUadder
    I have written no test cases so far :(

--- src/aarith/operations/approx_operations.hpp
+++ src/aarith/operations/approx_operations.hpp
@@ -1,9 +1,9 @@
 #pragma once
 
+#include "aarith/types/uinteger.hpp"
 #include <aarith/operations/exact_operations.hpp>
 #include <cstdint>
 #include <iostream>
-#include "aarith/types/uinteger.hpp"
 
 namespace aarith {
 
@@ -42,12 +42,11 @@ template <class UInteger> auto generate_bitmask(const size_t leading_ones) -> UI
 }
 
 template <class UInteger, class Function>
-[[nodiscard]] UInteger
-approx_operation_post_masking(const UInteger& a, const UInteger b,
-                              Function fun,
-                              const size_t bits = UInteger::width())
+[[nodiscard]] UInteger approx_operation_post_masking(const UInteger& a, const UInteger b,
+                                                     Function fun,
+                                                     const size_t bits = UInteger::width())
 {
-    const UInteger result = fun(a,b);
+    const UInteger result = fun(a, b);
     const UInteger mask = generate_bitmask<UInteger>(bits);
 
     return result & mask;
@@ -57,88 +56,87 @@ template <class UInteger>
 [[nodiscard]] UInteger approx_add_post_masking(const UInteger& a, const UInteger b,
                                                const size_t bits = UInteger::width())
 {
-    return approx_operation_post_masking(a,b,&aarith::add<UInteger::width()>,bits);
+    return approx_operation_post_masking(a, b, &aarith::add<UInteger::width()>, bits);
 }
 
 template <class UInteger>
 [[nodiscard]] UInteger approx_mul_post_masking(const UInteger& a, const UInteger b,
                                                const size_t bits = UInteger::width())
 {
-    return approx_operation_post_masking(a,b,&aarith::mul<UInteger::width()>,bits);
+    return approx_operation_post_masking(a, b, &aarith::mul<UInteger::width()>, bits);
 }
 
 template <class UInteger>
 [[nodiscard]] UInteger approx_sub_post_masking(const UInteger& a, const UInteger b,
                                                const size_t bits = UInteger::width())
 {
-    return approx_operation_post_masking(a,b,&aarith::sub<UInteger::width()>,bits);
+    return approx_operation_post_masking(a, b, &aarith::sub<UInteger::width()>, bits);
 }
 
 template <class UInteger>
 [[nodiscard]] UInteger approx_div_post_masking(const UInteger& a, const UInteger b,
                                                const size_t bits = UInteger::width())
 {
-    return approx_operation_post_masking(a,b,&aarith::div<UInteger>,bits);
+    return approx_operation_post_masking(a, b, &aarith::div<UInteger>, bits);
 }
 
 template <class UInteger>
 [[nodiscard]] UInteger approx_rem_post_masking(const UInteger& a, const UInteger b,
                                                const size_t bits = UInteger::width())
 {
-    return approx_operation_post_masking(a,b,&aarith::remainder<UInteger>,bits);
+    return approx_operation_post_masking(a, b, &aarith::remainder<UInteger>, bits);
 }
 
 template <class UInteger, class Function>
-[[nodiscard]] UInteger
-approx_operation_pre_masking(const UInteger& a, const UInteger b,
-                              Function fun,
-                              const size_t bits = UInteger::width())
+[[nodiscard]] UInteger approx_operation_pre_masking(const UInteger& a, const UInteger b,
+                                                    Function fun,
+                                                    const size_t bits = UInteger::width())
 {
     const UInteger mask = generate_bitmask<UInteger>(bits);
     auto const a_masked = a & mask;
     auto const b_masked = b & mask;
-
 
     return fun(a_masked, b_masked);
 }
 
 template <class UInteger>
 [[nodiscard]] UInteger approx_add_pre_masking(const UInteger& a, const UInteger b,
-                                               const size_t bits = UInteger::width())
+                                              const size_t bits = UInteger::width())
 {
-    return approx_operation_pre_masking(a,b,&aarith::add<UInteger::width()>,bits);
+    return approx_operation_pre_masking(a, b, &aarith::add<UInteger::width()>, bits);
 }
 
 template <class UInteger>
 [[nodiscard]] UInteger approx_mul_pre_masking(const UInteger& a, const UInteger b,
-                                               const size_t bits = UInteger::width())
+                                              const size_t bits = UInteger::width())
 {
-    return approx_operation_pre_masking(a,b,&aarith::mul<UInteger::width()>,bits);
+    return approx_operation_pre_masking(a, b, &aarith::mul<UInteger::width()>, bits);
 }
 
 template <class UInteger>
 [[nodiscard]] UInteger approx_sub_pre_masking(const UInteger& a, const UInteger b,
-                                               const size_t bits = UInteger::width())
+                                              const size_t bits = UInteger::width())
 {
-    return approx_operation_pre_masking(a,b,&aarith::sub<UInteger::width()>,bits);
+    return approx_operation_pre_masking(a, b, &aarith::sub<UInteger::width()>, bits);
 }
 
 template <class UInteger>
 [[nodiscard]] UInteger approx_div_pre_masking(const UInteger& a, const UInteger b,
-                                               const size_t bits = UInteger::width())
+                                              const size_t bits = UInteger::width())
 {
-    return approx_operation_pre_masking(a,b,&aarith::div<UInteger>,bits);
+    return approx_operation_pre_masking(a, b, &aarith::div<UInteger>, bits);
 }
 
 template <class UInteger>
 [[nodiscard]] UInteger approx_rem_pre_masking(const UInteger& a, const UInteger b,
-                                               const size_t bits = UInteger::width())
+                                              const size_t bits = UInteger::width())
 {
-    return approx_operation_pre_masking(a,b,&aarith::remainder<UInteger>,bits);
+    return approx_operation_pre_masking(a, b, &aarith::remainder<UInteger>, bits);
 }
 
 /**
- * @brief Multiplies the given most-significand portion of two uintegers by masking the partial products used in the multiplication
+ * @brief Multiplies the given most-significand portion of two uintegers by masking the partial
+ * products used in the multiplication
  *
  * @tparam Width The width of the used uintegers
  * @param opd1 Multiplier
@@ -165,6 +163,57 @@ auto approx_uint_bitmasking_mul(const uinteger<width>& opd1, const uinteger<widt
     }
 
     return product;
+}
+
+template <size_t width, size_t splitting_point>
+uinteger<width> FAUadder(const uinteger<width>& a, const uinteger<width>& b)
+{
+    const uinteger<splitting_point> a_lsp{width_cast<splitting_point>(a)};
+    const uinteger<splitting_point> b_lsp{width_cast<splitting_point>(b)};
+
+    std::cout << group_digits(to_binary(a),splitting_point) << "\n";
+    std::cout << group_digits(to_binary(b),splitting_point) << "\n";
+
+    std::cout << group_digits(to_binary(a_lsp),splitting_point) << "\n";
+    std::cout << group_digits(to_binary(b_lsp),splitting_point) << "\n";
+
+    uinteger<splitting_point + 1> lsp_sum=expanding_add(a_lsp,b_lsp);
+
+    std::cout << group_digits(to_binary(lsp_sum),splitting_point) << "\n";
+
+    uinteger<splitting_point> lsp = width_cast<splitting_point>(lsp_sum);
+    std::cout << group_digits(to_binary(lsp),splitting_point) << "\n";
+
+    // set everything to ones in case of a carry
+    const bool has_carry = lsp_sum.msb();
+
+
+    if (has_carry) {
+        lsp=lsp.all_ones();
+    }
+
+
+    const uinteger<width-splitting_point> a_msp{width_cast<width-splitting_point>(a >> splitting_point)};
+    const uinteger<width-splitting_point> b_msp{width_cast<width-splitting_point>(b >> splitting_point)};
+
+    std::cout << "lsp " << group_digits(to_binary(lsp),splitting_point) << "\n";
+
+    std::cout << group_digits(to_binary(a_msp),splitting_point) << "\n";
+    std::cout << group_digits(to_binary(b_msp),splitting_point) << "\n";
+
+    const auto msp = add(a_msp,b_msp, has_carry);
+
+    std::cout << "msp " <<  group_digits(to_binary(msp),splitting_point) << "\n";
+
+    uinteger<width> result{lsp};
+
+
+    std::cout << "res " << group_digits(to_binary(result),splitting_point) << "\n";
+
+    result = add(result, width_cast<width>(msp) << splitting_point);
+
+    std::cout << "res " << group_digits(to_binary(result),splitting_point) << "\n";
+    return result;
 }
 
 } // namespace aarith
---
+++ tests/uint-approx-test.cpp
@@ -0,0 +1,17 @@
+#include <aarith/operations/comparisons.hpp>
+#include <aarith/types/uinteger.hpp>
+#include <aarith/utilities/string_utils.hpp>
+#include <aarith/operations/approx_operations.hpp>
+#include <catch.hpp>
+#include <sstream>
+#include <iostream>
+
+using namespace aarith;
+
+SCENARIO("Using the FAU adder","[uinteger][arithmetic][approximate]")
+{
+    const uinteger<8> a{23};
+    const uinteger<8> b{1337};
+
+    FAUadder<8,3>(a,b);
+}
