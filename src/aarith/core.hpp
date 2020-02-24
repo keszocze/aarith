f52055795558435ee515f6e330a4b2ff3d1da2c5
Oliver Keszöcze <oliver.keszoecze@fau.de>
2020-02-24 19:35:42 +0100

    Add convenience header for the core

---
+++ src/aarith/core.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <aarith/core/word_container.hpp>
+#include <aarith/core/word_container_comparisons.hpp>
+
+#include <aarith/core/traits.hpp>
+
+#include <aarith/core/string_utils.hpp>
