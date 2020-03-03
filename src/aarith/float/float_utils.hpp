b192236c30f4f4cf579f9b6251184734a481b7f0
Oliver Keszöcze <oliver.keszoecze@fau.de>
2020-03-03 13:40:28 +0100

    Use memcpy to extract the bits of a float/double
    Fixes #42

--- src/aarith/float/float_utils.hpp
+++ src/aarith/float/float_utils.hpp
@@ -3,6 +3,7 @@
 #include <aarith/core/traits.hpp>
 
 #include <cstdint>
+#include <cstring>
 #include <type_traits>
 
 namespace aarith {
@@ -33,12 +34,26 @@ template <class F> constexpr auto get_exponent_width()
     }
 }
 
+template <class I, class F> I get_float_bits(const F f)
+{
+    /*
+     * We have to make sure that only the correct types are cast
+     */
+    static_assert((std::is_same_v<I, uint32_t> && std::is_same_v<F, float>) ||
+                  (std::is_same_v<I, uint64_t> && std::is_same_v<F, double>));
+
+    I value;
+    std::memcpy(&value, &f, sizeof(I));
+    return value;
+}
+
 inline auto extract_sign(float num) -> uint8_t
 {
     constexpr auto exponent_width = get_exponent_width<float>();
     constexpr auto mantissa_width = get_mantissa_width<float>();
 
-    auto const inum = *reinterpret_cast<uint32_t*>(&num);
+    uint32_t inum = get_float_bits<uint32_t>(num);
+
     auto sign = inum >> (exponent_width + mantissa_width);
     return static_cast<uint8_t>(sign);
 }
@@ -48,7 +63,8 @@ inline auto extract_sign(double num) -> uint8_t
     constexpr auto exponent_width = get_exponent_width<double>();
     constexpr auto mantissa_width = get_mantissa_width<double>();
 
-    auto const lnum = *reinterpret_cast<uint64_t*>(&num);
+    uint64_t lnum = get_float_bits<uint64_t>(num);
+
     auto sign = lnum >> (exponent_width + mantissa_width);
     return static_cast<uint8_t>(sign);
 }
@@ -58,7 +74,8 @@ inline auto extract_exponent(float num) -> unsigned int
     constexpr auto exponent_width = get_exponent_width<float>();
     constexpr auto mantissa_width = get_mantissa_width<float>();
 
-    const auto inum = *reinterpret_cast<uint32_t*>(&num);
+    uint32_t inum = get_float_bits<uint32_t>(num);
+
     const auto exponent = (inum >> mantissa_width) & ((1U << exponent_width) - 1);
 
     return static_cast<unsigned int>(exponent);
@@ -69,7 +86,8 @@ inline auto extract_exponent(double num) -> unsigned int
     constexpr auto exponent_width = get_exponent_width<double>();
     constexpr auto mantissa_width = get_mantissa_width<double>();
 
-    auto const lnum = *reinterpret_cast<uint64_t*>(&num);
+    uint64_t lnum = get_float_bits<uint64_t>(num);
+
     const auto exponent = (lnum >> mantissa_width) & ((1U << exponent_width) - 1);
 
     return static_cast<unsigned int>(exponent);
@@ -79,7 +97,8 @@ inline auto extract_mantissa(float num) -> uint32_t
 {
     constexpr auto mantissa_width = get_mantissa_width<float>();
 
-    auto const inum = *reinterpret_cast<uint32_t*>(&num);
+    uint32_t inum = get_float_bits<uint32_t>(num);
+
     const auto mantissa = (inum & ((1U << mantissa_width) - 1)) | (1U << mantissa_width);
 
     return static_cast<uint32_t>(mantissa);
@@ -89,8 +108,9 @@ inline auto extract_mantissa(double num) -> uint64_t
 {
     constexpr auto mantissa_width = get_mantissa_width<double>();
 
-    auto const inum = *reinterpret_cast<uint64_t*>(&num);
-    const auto mantissa = (inum & ((1ULL << mantissa_width) - 1)) | (1ULL << mantissa_width);
+    uint64_t lnum = get_float_bits<uint64_t>(num);
+
+    const auto mantissa = (lnum & ((1ULL << mantissa_width) - 1)) | (1ULL << mantissa_width);
 
     return static_cast<uint64_t>(mantissa);
 }
