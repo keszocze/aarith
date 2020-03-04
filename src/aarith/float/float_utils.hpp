18650856bd3e4ad7c2674d47637366624783dcd4
Oliver Keszöcze <oliver.keszoecze@fau.de>
2020-03-04 14:41:13 +0100

    Move bit_cast to separate file

--- src/aarith/float/float_utils.hpp
+++ src/aarith/float/float_utils.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <aarith/core/traits.hpp>
+#include <aarith/core/bit_cast.hpp>
 
 #include <cstdint>
 #include <cstring>
@@ -34,29 +35,7 @@ template <class F> constexpr auto get_exponent_width()
     }
 }
 
-/**
- * To avoid undefined behaviour when type punning, we are using memcpy.
- * This is rather annoying but seems to be the way to go. The implementation
- * is stolen from this talk:
- * https://www.youtube.com/watch?v=_qzMpk-22cc
- *
- * @todo replace with std::bit_cast when switching to C++20
- *
- * @tparam To The type to convert to
- * @tparam From The type to convert from
- * @param src The source of the bits
- * @return The new type filled with the bits of src
- */
-template <typename To, typename From,
-          typename =
-              std::enable_if_t<(sizeof(To) == sizeof(From)) && std::is_trivially_copyable_v<From> &&
-                               std::is_trivially_copyable_v<To>>>
-To bit_cast(const From& src) noexcept
-{
-    To dst;
-    std::memcpy(&dst, &src, sizeof(To));
-    return dst;
-}
+
 
 /**
  * @brief Namespace to prevent people from accidentally using this trait
