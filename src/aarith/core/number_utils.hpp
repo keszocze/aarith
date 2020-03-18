81007a48d43ba1dce697c7c298b9174cd3058ecd
Oliver Keszöcze <oliver.keszoecze@fau.de>
2020-03-18 15:03:56 +0100

    Add first_set_bit method to number utils

--- src/aarith/core/number_utils.hpp
+++ src/aarith/core/number_utils.hpp
@@ -21,4 +21,21 @@ template <class Result> constexpr Result ceil(double num)
                : static_cast<Result>(num) + ((num > 0) ? 1 : 0);
 }
 
+/**
+ * @brief Computes the position of the first set bit (i.e. a bit set to one) in the size_t from MSB
+ * to LSB
+ *
+ * @param n The size_t whose first set bit should be found
+ * @return The index of the first set bit in n
+ */
+[[nodiscard]] constexpr size_t first_set_bit(const size_t n)
+{
+    size_t first_bit = 0UL;
+    while (n >> first_bit)
+    {
+        ++first_bit;
+    }
+    return first_bit;
+}
+
 } // namespace aarith
