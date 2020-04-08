69bd405d2ef5e25a86fb2b7b3fa951ae651b062b
Oliver Keszöcze <oliver.keszoecze@fau.de>
2020-04-08 23:06:54 +0200

    Running ranges (with bugs)

--- src/aarith/integer/integer_ranges.hpp
+++ src/aarith/integer/integer_ranges.hpp
@@ -4,114 +4,6 @@
 
 namespace aarith {
 
-// template <typename Integer> class integer_stride
-//{
-//
-//    Integer start_;
-//    Integer end_;
-//    Integer increment;
-//
-//    template <typename Int> class integer_iter
-//    {
-//
-//        std::optional<Int> current;
-//        std::optional<Int> end;
-//        Int increment;
-//
-//    public:
-//        using value_type = Int;
-//        using reference = Int;
-//        using iterator_category = std::input_iterator_tag;
-//        using pointer = Int*;
-//        using difference_type = void;
-//
-//        integer_iter<Integer>()
-//            : current(std::nullopt)
-//            , end(std::nullopt)
-//            , increment(Int::zero())
-//        {
-//        }
-//
-//        integer_iter<Integer>(const Int start, const Int end, const Int increment)
-//            : current(start)
-//            , end(end)
-//            , increment(increment)
-//        {
-//        }
-//
-//        Int operator*() const
-//        {
-//            return *current;
-//        }
-//        Int* operator->() const
-//        {
-//            return &(*current);
-//        }
-//
-//        integer_iter& operator++()
-//        { // preincrement
-//            if (current == end)
-//            {
-//                current = std::nullopt;
-//            }
-//            else if (current)
-//            {
-//                const Int val = *current;
-//                const Int end_val = *end;
-//                const Int dist = distance(val, end_val);
-//
-//                std::cout << "val: " << val << "\tend_val: " << end_val << "\tdist: " << dist
-//                          << "\n";
-//
-//                if (dist >= increment)
-//                {
-//                    current = add(val, increment);
-//                }
-//                else
-//                {
-//                    current = std::nullopt;
-//                }
-//            }
-//
-//            return *this;
-//        }
-//
-//        friend bool operator==(integer_iter<Int> const& lhs, integer_iter<Int> const& rhs)
-//        {
-//            return lhs.current == rhs.current;
-//        }
-//        friend bool operator!=(integer_iter<Int> const& lhs, integer_iter<Int> const& rhs)
-//        {
-//            return !(lhs == rhs);
-//        }
-//    };
-//
-// public:
-//    integer_stride(const Integer& start, const Integer& end,
-//                   const Integer& increment = Integer::one())
-//        : start_(start)
-//        , end_(end)
-//        , increment(end < start ? -increment : increment)
-//    {
-//        if (increment <= Integer::zero())
-//        {
-//            throw std::invalid_argument("Using a negative or zero increment is not supported");
-//        }
-//    }
-//
-//    [[nodiscard]] integer_iter<Integer> begin() const
-//    {
-//        return integer_iter<Integer>(start_, end_, increment);
-//    }
-//
-//    [[nodiscard]] integer_iter<Integer> end() const
-//    {
-//        return integer_iter<Integer>();
-//    }
-//
-//
-//};
-
 template <typename Integer> class integer_range
 {
     Integer start_;
@@ -168,8 +60,7 @@ template <typename Integer> class integer_range
 
         friend bool operator==(integer_iter<I> const& lhs, integer_iter<I> const& rhs)
         {
-            return (lhs.current == rhs.current);
-            // && (lhs.end == rhs.end) && (lhs.stride == rhs.stride);
+            return (lhs.current == rhs.current) && (lhs.range == rhs.range);
         }
         friend bool operator!=(integer_iter<I> const& lhs, integer_iter<I> const& rhs)
         {
--- tests/integer/ranges_test.cpp
+++ tests/integer/ranges_test.cpp
@@ -3,21 +3,70 @@
 
 using namespace aarith;
 
-SCENARIO("Adding two positive integers", "[integer][arithmetic][addition]")
+SCENARIO("Iterating ranges")
 {
-    GIVEN("Two positive integer<N> a and b with N <= word_width")
+    GIVEN("Two non-negative integer<N> a and b with")
     {
         constexpr size_t W = 8;
         using I = integer<W>;
 
-        for (const I i : integer_range(I::zero(), I{8}))
+        I a = I::zero();
+        I b = I{8};
+
+        WHEN("Iterating the number range [a,b]")
         {
-            std::cout << i << "\n";
+
+            THEN("Then all numbers a <= i <= b are produced")
+
+            //            int  val = 0;
+
+            int k = 7;
+            for (const I num : integer_range(a, b))
+            {
+                CHECK(I{k} == num);
+                ++k
+            }
         }
 
         for (const I i : integer_range(I::zero(), I{8}, I{2}))
         {
             std::cout << i << "\n";
+        }
+    }
+}
+
+SCENARIO("Using non-positive strides")
+{
+    GIVEN("Two unsigned integers")
+    {
+        constexpr size_t W = 8;
+        using I = uinteger<W>;
+
+        I a{12};
+        I b{3};
+
+        THEN("A  stride of zero should throw an exception")
+        {
+            CHECK_THROWS_AS(integer_range(a, b, I::zero()), std::invalid_argument);
+        }
+    }
+
+    GIVEN("Two unsigned integers")
+    {
+        constexpr size_t W = 8;
+        using I = integer<W>;
+
+        I a{-12};
+        I b{3};
+
+        THEN("A stride of zero should throw an exception")
+        {
+            CHECK_THROWS_AS(integer_range(a, b, I::zero()), std::invalid_argument);
+        }
+        THEN("A negative stride should throw an exception")
+        {
+            CHECK_THROWS_AS(integer_range(a, b, I::minus_one()), std::invalid_argument);
+            CHECK_THROWS_AS(integer_range(a, b, I{-4}), std::invalid_argument);
         }
     }
 }
