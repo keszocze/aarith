3a2a604419e99600ce9379faac350e7f6e07d8de
Oliver Keszöcze <oliver.keszoecze@fau.de>
2020-04-09 17:28:46 +0200

    Greatly simplify the code of operator== for integers
    This is most likely running slower than before, but as long as we
    do not have any profiling info that complains about this, we are
    going to keep it this way.

--- src/aarith/integer/integer_comparisons.hpp
+++ src/aarith/integer/integer_comparisons.hpp
@@ -133,102 +133,24 @@ template <typename W, typename V> bool operator>(const W& a, const V& b)
 
 template <size_t W, size_t V> bool operator==(const integer<W>& a, const integer<V>& b)
 {
-    // TODO remove when clang implements the stuff as constexpr
-#ifndef __clang__
-    constexpr auto min_count = std::min(a.word_count(), b.word_count());
-    constexpr auto max_count = std::max(a.word_count(), b.word_count());
-#else
-    const auto min_count = std::min(a.word_count(), b.word_count());
-    const auto max_count = std::max(a.word_count(), b.word_count());
-#endif
 
     if (a.is_negative() != b.is_negative())
     {
         return false;
     }
 
-    const bool numbers_are_negative = a.is_negative(); // we can pick either a or b
+    constexpr size_t max_width = std::max(W, V);
+
+    integer<max_width> a_ = width_cast<max_width>(a);
+    integer<max_width> b_ = width_cast<max_width>(b);
 
     using word_type = typename integer<W>::word_type;
 
-    if constexpr (W > V)
+    for (size_t i = 0U; i < integer<max_width>::word_count(); ++i)
     {
-
-        for (size_t i = min_count; i < max_count; ++i)
+        if (a_.word(i) != b_.word(i))
         {
-            /*
-             * We have to check whether the leading bits (within the range of the mask/the number of
-             * specified bits) consists of ones only. This is necessary for negative numbers only
-             */
-            if (numbers_are_negative)
-            {
-                const word_type mask = integer<W>::word_mask(i);
-                if (a.word(i) != mask)
-                {
-                    return false;
-                }
-            }
-            else
-            {
-                /*
-                 * If the numbers are not negative, there is no need to check against ones (as
-                 * provided by the mask) being set as the unused bits are already correctly filled
-                 * with zeroes (at least, if the numbers are identical)
-                 */
-                if (a.word(i) != 0U)
-                {
-                    return false;
-                }
-            };
-        }
-    }
-    else
-    {
-        for (size_t i = min_count; i < max_count; ++i)
-        {
-            if (numbers_are_negative)
-            {
-                const word_type mask = integer<V>::word_mask(i);
-                if (b.word(i) != mask)
-                {
-                    return false;
-                }
-            }
-            else
-            {
-                if (b.word(i) != 0U)
-                {
-                    return false;
-                }
-            };
-        }
-    }
-
-    for (auto i = 0U; i < min_count; ++i)
-    {
-        if (numbers_are_negative)
-        {
-            if constexpr (W > V)
-            {
-                if ((a.word(i) & b.word_mask(i)) != b.word(i))
-                {
-                    return false;
-                }
-            }
-            else
-            {
-                if (a.word(i) != (b.word(i) & a.word_mask(i)))
-                {
-                    return false;
-                }
-            }
-        }
-        else
-        {
-            if (a.word(i) != b.word(i))
-            {
-                return false;
-            }
+            return false;
         }
     }
 
