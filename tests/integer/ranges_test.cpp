d45ca20a322e12a8df377a8d9764d99f4acf0296
Oliver Keszöcze <oliver.keszoecze@fau.de>
2020-04-11 13:38:55 +0200

    Add test cases for the unsigned signum function

--- tests/integer/ranges_test.cpp
+++ tests/integer/ranges_test.cpp
@@ -45,8 +45,8 @@ SCENARIO("Iterating ranges")
         {
             THEN("There is no overflow")
             {
-                const std::array<int,3> nums{0,3,6};
-                int k=0;
+                const std::array<int, 3> nums{0, 3, 6};
+                int k = 0;
                 for (const I i : integer_range(a, b, I{3}))
                 {
                     REQUIRE(I{nums[k]} == i);
@@ -60,16 +60,19 @@ SCENARIO("Iterating ranges")
     GIVEN("Two integers a and b with a < 0 < b")
     {
 
-        const I a{-1};
-        const I b{1};
+        const I a{-5};
+        const I b{5};
 
         WHEN("They form a range")
         {
             THEN("The iterations works fine")
             {
+
+                int k = -5;
                 for (const I i : integer_range(a, b, I{1}))
                 {
-                    std::cout << i << "\n";
+                    REQUIRE(I{k} == i);
+                    ++k;
                 }
             }
         }
@@ -84,12 +87,12 @@ SCENARIO("Iterating ranges")
             THEN("The range should be empty")
             {
 
-                std::cout << "Iterating over an empty range:\n";
-                for (const I num : integer_range(a, b))
+                size_t count = 0;
+                for ([[maybe_unused]] const I num : integer_range(a, b))
                 {
-                    std::cout << num << "\n";
+                    ++count;
                 }
-                std::cout << "\n";
+                REQUIRE(count == 0);
             }
         }
     }
@@ -101,10 +104,12 @@ SCENARIO("Iterating ranges")
         {
             THEN("There should be one value, namely a")
             {
-                for (const I num : integer_range(a, a))
+                size_t count = 0;
+                for ([[maybe_unused]] const I num : integer_range(a, a))
                 {
-                    std::cout << num << "\n";
+                    ++count;
                 }
+                REQUIRE(count == 1);
             }
         }
     }
@@ -126,7 +131,7 @@ SCENARIO("Using non-positive strides")
         }
     }
 
-    GIVEN("Two unsigned integers")
+    GIVEN("Two integers")
     {
         constexpr size_t W = 8;
         using I = integer<W>;
