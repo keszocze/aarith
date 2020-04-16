77557a40f72b800de9a6e86c941e1ccb3046d909
Oliver Keszöcze <oliver.keszoecze@fau.de>
2020-04-16 19:59:21 +0200

    Fix coverage path

--- scripts/mk_sonar_config.sh
+++ scripts/mk_sonar_config.sh
@@ -23,7 +23,7 @@ cat <<EOF
 	$(branch_config $1)
 
 	sonar.sourceEncoding=UTF-8
-	sonar.coverageReportPaths=./build/tests/coverage/coverage.xml
+	sonar.coverageReportPaths=./build/tests/gcc/coverage/coverage.xml
 
 	sonar.host.url=https://codesignp211.informatik.uni-erlangen.de/sonarqube
 EOF
