af421e82342d4bd34ae9b868b9f2bb371bed7630
Oliver Keszöcze <oliver.keszoecze@fau.de>
2020-05-14 11:46:36 +0200

    allow to select which eval fau adder experiments to run via arguments

--- experiments/eval_fau_adder.cpp
+++ experiments/eval_fau_adder.cpp
@@ -4,9 +4,9 @@
 
 #include "../tests/integer/fau_adder.hpp"
 
-template <size_t width, size_t lsp_width, size_t shared_bits = 0>
-void show_fau_adder_evaluation() {
-    const auto [AER,MED, ME] = aarith::eval_fau_adder<width, lsp_width, shared_bits>();
+template <size_t width, size_t lsp_width, size_t shared_bits = 0> void show_fau_adder_evaluation()
+{
+    const auto [AER, MED, ME] = aarith::eval_fau_adder<width, lsp_width, shared_bits>();
 
     std::cout << "Results for evaluating the FAU adder for \n";
     std::cout << "\ta bit width of " << width << "\n";
@@ -18,21 +18,27 @@ void show_fau_adder_evaluation() {
     std::cout << "\t2^" << 2 * width << "* MED = " << MED << "\n";
 }
 
-int main()
+int main(int argc, char** argv)
 {
     using namespace aarith;
 
-    show_fau_adder_evaluation<8, 4, 1>();
-    std::cout << "\n\n";
-    show_fau_adder_evaluation<8, 4, 3>();
-    std::cout << "\n\n";
-    show_fau_adder_evaluation<16, 8, 1>();
-    std::cout << "\n\n";
-    show_fau_adder_evaluation<16, 8, 3>();
-    std::cout << "\n\n";
-//    show_fau_adder_evaluation<32, 16, 1>();
-//    std::cout << "\n\n";
-//    show_fau_adder_evaluation<32, 16, 3>();
+    if (argc == 1)
+    {
+        show_fau_adder_evaluation<8, 4, 1>();
+        std::cout << "\n\n";
+        show_fau_adder_evaluation<8, 4, 3>();
+        std::cout << "\n\n";
+        show_fau_adder_evaluation<16, 8, 1>();
+        std::cout << "\n\n";
+        show_fau_adder_evaluation<16, 8, 3>();
+        std::cout << "\n\n";
+    }
+    else
+    {
+        show_fau_adder_evaluation<32, 16, 1>();
+        std::cout << "\n\n";
+        show_fau_adder_evaluation<32, 16, 3>();
+    }
 
     return 0;
 }
