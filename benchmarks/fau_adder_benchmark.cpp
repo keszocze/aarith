50a4dac35093a327755005f7199eac9f25eaff58
Oliver Keszöcze <oliver.keszoecze@fau.de>
2020-05-14 11:42:47 +0200

    Fix fau_adder benchmark

--- benchmarks/fau_adder_benchmark.cpp
+++ benchmarks/fau_adder_benchmark.cpp
@@ -7,11 +7,13 @@ template <size_t width, size_t lsp_width, size_t shared_bits = 0>
 void wrapper(benchmark::State& state)
 {
     using T = std::tuple<aarith::uinteger<2 * width>, aarith::uinteger<2 * width>,
-               aarith::uinteger<2 * width>>;
+                         aarith::uinteger<2 * width>>;
+
+    T res;
 
     for (auto _ : state)
     {
-        benchmark::DoNotOptimize(aarith::eval_fau_adder<width, lsp_width, shared_bits>);
+        benchmark::DoNotOptimize(res = aarith::eval_fau_adder<width, lsp_width, shared_bits>());
     }
 }
 
@@ -19,12 +21,21 @@ int main(int argc, char** argv)
 {
     // TODO runs super fast -> something is weirdly optimized away
     using namespace aarith;
-    benchmark::RegisterBenchmark("FAU Adder n=8, m=4, p=1", &wrapper<8, 4, 1>);
-    benchmark::RegisterBenchmark("FAU Adder n=8, m=4, p=3", &wrapper<8, 4, 3>);
-    benchmark::RegisterBenchmark("FAU Adder n=16, m=8, p=1", &wrapper<16, 8, 1>);
-    benchmark::RegisterBenchmark("FAU Adder n=16, m=8, p=3", &wrapper<16, 8, 3>);
-    benchmark::RegisterBenchmark("FAU Adder n=32, m=16, p=1", &wrapper<32, 16, 1>);
-    //    benchmark::RegisterBenchmark("FAU Adder n=8, m=4, p=1", &wrapper<32,16,3>);
+    benchmark::RegisterBenchmark("FAU Adder n=8, m=4, p=1", &wrapper<8, 4, 1>)
+        ->Unit(benchmark::kMillisecond)
+        ->DisplayAggregatesOnly()
+        ->Repetitions(5);
+    benchmark::RegisterBenchmark("FAU Adder n=8, m=4, p=3", &wrapper<8, 4, 3>)
+        ->Unit(benchmark::kMillisecond)
+        ->DisplayAggregatesOnly()
+        ->Repetitions(5);
+    benchmark::RegisterBenchmark("FAU Adder n=16, m=8, p=1", &wrapper<16, 8, 1>)
+        ->Unit(benchmark::kMillisecond);
+    benchmark::RegisterBenchmark("FAU Adder n=16, m=8, p=3", &wrapper<16, 8, 3>)
+        ->Unit(benchmark::kMillisecond);
+    benchmark::RegisterBenchmark("FAU Adder n=32, m=16, p=1", &wrapper<32, 16, 1>)
+        ->Unit(benchmark::kMillisecond);
+        benchmark::RegisterBenchmark("FAU Adder n=8, m=4, p=1", &wrapper<32,16,3>);
 
     benchmark::Initialize(&argc, argv);
     benchmark::RunSpecifiedBenchmarks();
