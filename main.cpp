d76adc6c866998d9e5faa84c9a0f01313ac3ac5c
Oliver Keszöcze <oliver.keszoecze@fau.de>
2020-05-15 15:07:10 +0200

    Create code for output comparison

--- main.cpp
+++ main.cpp
@@ -5,6 +5,14 @@
 int main()
 {
     using namespace aarith;
-    std::cout << (std::numeric_limits<uinteger<13>>::is_exact ? "T" : "F") << std::endl;
+
+    for (int i = 0; i < 50; ++i)
+    {
+
+        uint8_t u8 = 42;
+        uinteger<8> aarith_u8{u8};
+        std::cout << "uint8_t=" << u8 << " (as int=" << int{u8}
+                  << ") aarith::uinteger<8>=" << aarith_u8 << "\n";
+    }
     return 0;
 }
