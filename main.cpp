5aee472cc13ce476bc0085bc374c286415727ffb
Oliver Keszöcze <oliver.keszoecze@fau.de>
2020-06-12 11:26:14 +0200

    Extend test case

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@
 int main()
 {
     using namespace aarith;
-    using namespace aarith::arithmetic_operators;
+//    using namespace aarith::arithmetic_operators;
     using nf_t = normalized_float<4, 4>;
 
     nf_t nf_a(1.5f);
@@ -23,7 +23,14 @@ int main()
     nf_vec.push_back(nf_a);
     nf_vec.push_back(nf_b);
 
+    const auto acc = std::accumulate(nf_vec.begin(), nf_vec.end(), nf_zero);
+    const auto normal = add(nf_a, nf_b );
+    const auto op = nf_a + nf_b;
+
+    const bool all_equal = (acc == normal) && (acc == op) && (normal == op);
+
     std::cout << "A: " << nf_vec[0] << " B: " << nf_vec[1] << std::endl
-              << std::accumulate(nf_vec.begin(), nf_vec.end(), nf_zero);
+              << "accumulated: " << acc  << "\t" << "add function: " << add(nf_a, nf_b) << "\t" << "plus operator: " << op << "\n";
+    std::cout << "All equal: " << all_equal << "\n";
     return 0;
 }
