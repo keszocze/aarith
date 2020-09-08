c011f1215cc6f621ed6e93e21effd737f8a4ebc2
Oliver Keszöcze <oliver.keszoecze@fau.de>
2020-09-08 10:22:23 +0200

    Remove some clang warnings

--- main.cpp
+++ main.cpp
@@ -13,59 +13,6 @@
 using nf_t = aarith::normalized_float<3, 5>;
 using snf = aarith::normalized_float<8, 23>;
 
-// something like this would be useful in aarith
-// using newly committed conversions here
-template <size_t exp_bits, size_t man_bits> inline float to_float(nf_t x)
-{
-    std::cout << "to_float: M=" << x.get_mantissa() << " E=" << x.get_exponent()
-              << " S=" << x.get_sign() << " B=" << x.get_bias() << std::endl;
-    const size_t float_man_len = 23;
-    const size_t float_exp_len = 8;
-    const int float_bias = 127;
-
-    float res;
-    uint32_t float_rep = 0U;
-
-    if (x.get_sign())
-        // set sign bit
-        float_rep |= (1 << 31);
-
-    snf anf(-93.211546f);
-    snf bnf(0.f);
-
-    add(anf, bnf);
-
-    uint32_t mantissa_part;
-    // get them bits
-    mantissa_part = static_cast<uint32_t>(x.get_mantissa());
-    // truncate to given size - maybe we can omit this?
-    mantissa_part &= ((1 << man_bits) - 1);
-    // adjust mantissa with left shift
-    static_assert(man_bits <= float_man_len);
-    // make old format MSBs to new format MSBs
-    mantissa_part <<= (float_man_len - man_bits);
-    float_rep &= mantissa_part;
-
-    uint32_t exponent_part, exponent_bias;
-    // same for exponent
-    exponent_part = static_cast<uint32_t>(x.get_exponent());
-    exponent_bias = static_cast<uint32_t>(x.get_bias());
-    exponent_part &= ((1 << exp_bits) - 1);
-    // remove old biasing
-    exponent_part -= exponent_bias;
-    // rebias for float32
-    exponent_part += float_bias;
-    static_assert(exp_bits <= float_exp_len);
-    // CAREFUL - skip mantissa bits
-    exponent_part <<= float_man_len;
-    float_rep &= exponent_part;
-
-    // bitwise manipulation with different types sucks
-    // for now work around using std::memcpy
-    // compiler should optimize this
-    std::memcpy(&res, &float_rep, sizeof(float));
-    return res;
-}
 
 int main()
 {
