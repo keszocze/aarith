7a735dabac25e602846ed0de0b7c3b13f003d1d6
Oliver Keszöcze <oliver.keszoecze@fau.de>
2020-09-09 15:48:32 +0200

    add example

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
-#include <aarith/float.hpp>
+#include <aarith/float_no_operators.hpp>
 #include <iostream>
 #include <numeric>
 #include <vector>
+
+namespace aarith {
+template <size_t E, size_t M, typename WordType>
+auto operator+(const normalized_float<E, M, WordType>& lhs,
+               const normalized_float<E, M, WordType>& rhs) -> normalized_float<E, M, WordType>
+{
+    return add(lhs, rhs);
+}
+} // namespace aarith
 
 int main()
 {
