97284d60e2f8b2a80bfcb9d36a2c591788a44217
Oliver Keszöcze <oliver.keszoecze@fau.de>
2020-09-09 15:02:44 +0200

    update example for the gitlab issue

--- main.cpp
+++ main.cpp
@@ -6,7 +6,6 @@
 int main()
 {
     using namespace aarith;
-    using namespace aarith::float_operators;
 
     using nf_t = normalized_float<4, 4>;
 
@@ -19,18 +18,20 @@ int main()
     nf_vec.push_back(nf_b);
 
     const auto acc = std::accumulate(nf_vec.begin(), nf_vec.end(), nf_zero,
-                                     [](const nf_t acc, const nf_t & num) {
-                                         return acc + num;
-                                     });
-    const auto non_working_acc = std::accumulate(nf_vec.begin(), nf_vec.end(), nf_zero);
+                                     [](const nf_t acc, const nf_t& num) { return acc + num; });
+    const auto now_working_acc = std::accumulate(nf_vec.begin(), nf_vec.end(), nf_zero);
 
-    const auto normal = add(nf_a, nf_b );
+    const auto normal = add(nf_a, nf_b);
     const auto op = nf_a + nf_b;
 
-    const bool all_equal = (acc == normal) && (acc == op) && (normal == op);
+    const bool all_equal =
+        (acc == normal) && (acc == op) && (normal == op) && (now_working_acc == normal);
 
     std::cout << "A: " << nf_vec[0] << " B: " << nf_vec[1] << std::endl
-              << "accumulated: " << acc  << "\t" << "add function: " << add(nf_a, nf_b) << "\t" << "plus operator: " << op << "\n";
+              << "accumulated: " << acc << "\t"
+              << "fancy accumulated: " << now_working_acc << "\t"
+              << "add function: " << add(nf_a, nf_b) << "\t"
+              << "plus operator: " << op << "\n";
     std::cout << "All equal: " << all_equal << "\n";
     return 0;
 }
