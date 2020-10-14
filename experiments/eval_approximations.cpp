2477e8f3b78899c4f32b3141c3a42c96684bc23d
Oliver Keszöcze <oliver.keszoecze@fau.de>
2020-10-14 15:48:51 +0200

    Change full test range a bit

--- experiments/eval_approximations.cpp
+++ experiments/eval_approximations.cpp
@@ -13,8 +13,8 @@ using F = normalized_float<8, 23>;
 template <size_t A, typename WordType = uint64_t> void test_a()
 {
 
-    constexpr size_t N = 1;
-    constexpr size_t NDenorm = 1;
+    constexpr size_t N = 200;
+    constexpr size_t NDenorm = 100;
 
     std::random_device rd;
     std::mt19937 mt(rd());
@@ -58,6 +58,8 @@ template <size_t A, typename WordType = uint64_t> void test_a()
             F pos(false, e, m);
             F neg(true, e, m);
 
+            std::cout << to_binary(pos) << "\n";
+
             values.push_back(pos);
             values.push_back(neg);
         }
@@ -76,27 +78,28 @@ template <size_t A, typename WordType = uint64_t> void test_a()
               << "\n";
     for (const F& f : vals_a)
     {
+        const float f_float = static_cast<float>(f);
+
         for (const F& g : vals_b)
         {
 
-            float f_float = static_cast<float>(f);
-            float g_float = static_cast<float>(g);
+            const float g_float = static_cast<float>(g);
 
-            float add_float = f_float + g_float;
-            float sub_float = f_float - g_float;
-            float mul_float = f_float * g_float;
+            const float add_float = f_float + g_float;
+            const float sub_float = f_float - g_float;
+            const float mul_float = f_float * g_float;
 
-            Float add_float_F{add_float};
-            Float sub_float_F{sub_float};
-            Float mul_float_F{mul_float};
+            const Float add_float_F{add_float};
+            const Float sub_float_F{sub_float};
+            const Float mul_float_F{mul_float};
 
-            F add = f + g;
-            F sub = f - g;
-            F mul = f * g;
+            const F add = f + g;
+            const F sub = f - g;
+            const F mul = f * g;
 
-            F any_add = anytime_add(f, g, A + 1);
-            F any_sub = anytime_sub(f, g, A + 1);
-            F any_mul = anytime_mul(f, g, A + 1);
+            const F any_add = anytime_add(f, g, A + 1);
+            const F any_sub = anytime_sub(f, g, A + 1);
+            const F any_mul = anytime_mul(f, g, A + 1);
 
             std::cout << A << ";" << f_float << ";" << g_float << ";" << as_word_array<8, A>(f)
                       << ";" << as_word_array<8, A>(g) << ";";
@@ -121,28 +124,28 @@ template <size_t A, typename WordType = uint64_t> void test_a()
 int main()
 {
 
-    test_a<23>();
-    test_a<22>();
-    test_a<21>();
-    test_a<20>();
-    test_a<19>();
-    test_a<18>();
-    test_a<17>();
-    test_a<16>();
-    test_a<15>();
-    test_a<14>();
-    test_a<13>();
-    test_a<12>();
-    test_a<11>();
-    test_a<10>();
-    test_a<9>();
-    test_a<8>();
-    test_a<7>();
-    test_a<6>();
-    test_a<5>();
-    test_a<4>();
-    test_a<3>();
-    test_a<2>();
+        test_a<23>();
+        test_a<22>();
+        test_a<21>();
+        test_a<20>();
+        test_a<19>();
+        test_a<18>();
+        test_a<17>();
+        test_a<16>();
+        test_a<15>();
+        test_a<14>();
+        test_a<13>();
+        test_a<12>();
+        test_a<11>();
+        test_a<10>();
+        test_a<9>();
+        test_a<8>();
+        test_a<7>();
+        test_a<6>();
+        test_a<5>();
+        test_a<4>();
+        test_a<3>();
+        test_a<2>();
     test_a<1>();
     return EXIT_SUCCESS;
 }
