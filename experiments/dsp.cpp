dbb70d9bfe01dab62bdb43bab883fa70b212590b
Oliver Keszöcze <oliver.keszoecze@fau.de>
2020-11-18 18:46:01 +0100

    Extend DSP packing test stuff

--- experiments/dsp.cpp
+++ experiments/dsp.cpp
@@ -20,13 +20,14 @@ using auint4 = uinteger<4>;
 
     const DSPAddIn B_{B};
 
+    // TODO ask Akif about preferred automatic width extension
     const DSPRes prod = sum * B_;
 
     return prod;
 }
 
-[[nodiscard]] std::array<aint8, 4> pack_dsp(const aint4& w1, const aint4& w2,
-                                                      const auint4& a1, const auint4& a2)
+[[nodiscard]] std::array<aint8, 4> pack_dsp(const aint4& w1, const aint4& w2, const auint4& a1,
+                                            const auint4& a2)
 {
 
     const DSPAddIn A{w1};
@@ -38,40 +39,59 @@ using auint4 = uinteger<4>;
     A2 <<= 11;
     const DSPProdIn B = A1 + A2;
 
-    std::cout << A << " " << to_binary(A) << "\n";
-    std::cout << D << " " << to_binary(D) << "\n";
-    std::cout << B << " " << to_binary(B) << "\n";
+    const DSPRes dsp_res = dsp(A, D, B);
 
-    const DSPRes dsp_res = dsp(A,D,B);
+    const aint8 a1w1{bit_range<7, 0>(dsp_res)};
+    const aint8 a2w1{bit_range<18, 11>(dsp_res)};
+    const aint8 a1w2{bit_range<29, 22>(dsp_res)};
+    const aint8 a2w2{bit_range<40, 33>(dsp_res)};
 
-    const aint8 a1w1{bit_range<7,0>(dsp_res)};
-    const aint8 a2w1{bit_range<18,11>(dsp_res)};
-    const aint8 a1w2{bit_range<29,22>(dsp_res)};
-    const aint8 a2w2{bit_range<40,33>(dsp_res)};
+    return {a2w2, a1w2, a2w1, a1w1};
+}
 
-    std::cout << dsp_res << " " << to_binary(dsp_res) << "\n";
+bool test_single_packing(const aint4& w1, const aint4& w2, const auint4& a1, const auint4& a2)
+{
 
-    std::cout << "a1w1 " <<  bit_range<7,0>(dsp_res) << "\n";
-    std::cout << "a2w1 " <<  bit_range<18,11>(dsp_res) << "\n";
-    std::cout << "a1w2 " <<  bit_range<29,22>(dsp_res) << "\n";
-    std::cout << "a2w2 " <<  bit_range<40,33>(dsp_res) << "\n";
+    aint8 w1_{w1};
+    aint8 w2_{w2};
 
-    return {a2w2,a1w2,a2w1,a1w1};
+    aint8 a1_{a1};
+    aint8 a2_{a2};
+
+    const std::array<aint8, 4> res_correct = {a2_ * w2_, a1_ * w2_, a2_ * w1_, a1_ * w1_};
+
+    const std::array<aint8, 4> res_dsp = pack_dsp(w1, w2, a1, a2);
+
+    bool error = false;
+
+    for (size_t i = 0; i < 4; ++i)
+    {
+        error |= res_correct[i] != res_dsp[i];
+        std::cout << res_correct[i] << ";" << res_dsp[i] << ";" << (res_correct[i] - res_dsp[i]) << ";";
+
+    }
+
+    std::cout << "\n";
+
+    return error;
+}
+
+void test_packing() {
+    for (aint4 w1: integer_range<aint4>(aint4::min(),6)) {
+        std::cout << w1 << "\n";
+    }
 }
 
 int main()
 {
 
     const aint4 w1(-8);
-    const aint4 w2=aint4::zero();
+    const aint4 w2 = aint4::zero();
 
     const auint4 a1 = auint4::one();
     const auint4 a2 = auint4::one();
 
-    const auto [r1, r2, r3, r4] = pack_dsp(w1, w2, a1, a2);
-
-    std::cout << w1 << "\t" << w2 << "\t" << a1 << "\t" << a2 << "\t" << r1 << "\t" << r2 << "\t"
-              << r3 << "\t" << r4 << "\n";
-
+    test_single_packing(w1,w2,a1,a2);
+    test_packing();
     return 0;
 }
