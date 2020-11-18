6f958bdb8e2e4cb5b93ee99f0f27cbef6b2ec12a
Oliver Keszöcze <oliver.keszoecze@fau.de>
2020-11-18 21:18:38 +0100

    Finish Xilinx' and Akif's approach

--- experiments/dsp.cpp
+++ experiments/dsp.cpp
@@ -13,6 +13,25 @@ using aint8 = integer<8>;
 using aint4 = integer<4>;
 using auint4 = uinteger<4>;
 
+/**
+ * @brief Struct storing the inputs to the DSP
+ */
+struct DSPIn
+{
+    DSPAddIn A;                //! First of the summands
+    DSPAddIn D;                //! Second of the summands
+    DSPProdIn B;               //!
+    DSPRes C = DSPRes::zero(); //! The optional carry
+};
+
+/**
+ * @brief Models the DSP's behaviour
+ * @param A The first summand
+ * @param D The second summand
+ * @param B The other part of the product
+ * @param C The carry
+ * @return ((A+D)*B)+C
+ */
 [[nodiscard]] constexpr DSPRes dsp(const DSPAddIn& A, const DSPAddIn& D, const DSPProdIn& B,
                                    const DSPRes C = DSPRes::zero())
 {
@@ -29,10 +48,29 @@ using auint4 = uinteger<4>;
     return result;
 }
 
-[[nodiscard]] std::array<aint8, 4> pack_dsp_xilinx(const aint4& w1, const aint4& w2,
-                                                   const auint4& a1, const auint4& a2)
+/**
+ * @brief Helper function calling the DSP with a given input struct
+ * @param input The input parameters to the DSP packed in a strut
+ * @return ((A+D)*B)+C
+ */
+[[nodiscard]] constexpr DSPRes dsp(const DSPIn& input)
 {
+    return dsp(input.A, input.D, input.B, input.C);
+}
 
+/**
+ * @brief Packs the two signed weights and two unsigned activation values as proposed by Xilinx
+ *
+ * Note that this packing is incorrect
+ *
+ * @param w1 Weight 1
+ * @param w2 Weight 2
+ * @param a1 Activation value 1
+ * @param a2 Activation value 2
+ * @return Input packed so that it can be fed to the DSP
+ */
+DSPIn pack_xilinx(const aint4& w1, const aint4& w2, const auint4& a1, const auint4& a2)
+{
     const DSPAddIn A{w1};
     DSPAddIn D{w2};
     D <<= 22;
@@ -42,17 +80,69 @@ using auint4 = uinteger<4>;
     A2 <<= 11;
     const DSPProdIn B = A1 + A2;
 
-    const DSPRes dsp_res = dsp(A, D, B);
+    return {A, D, B};
+}
 
-    const aint8 a1w1{bit_range<7, 0>(dsp_res)};
-    const aint8 a2w1{bit_range<18, 11>(dsp_res)};
-    const aint8 a1w2{bit_range<29, 22>(dsp_res)};
-    const aint8 a2w2{bit_range<40, 33>(dsp_res)};
+/**
+ * @brief Packs the two signed weights and two unsigned activation values as proposed by Akif
+ *
+ * The idea is to perform a simple error correction using the carry input of the DSP. The packing
+ * of the other ports is as done by Xilinx.
+ *
+ * @param w1 Weight 1
+ * @param w2 Weight 2
+ * @param a1 Activation value 1
+ * @param a2 Activation value 2
+ * @return Input packed so that it can be fed to the DSP
+ */
+// TODO Check with Akif that this actually is what he proposed
+DSPIn pack_akif(const aint4& w1, const aint4& w2, const auint4& a1, const auint4& a2)
+{
+    DSPIn params = pack_xilinx(w1, w2, a1, a2);
+
+    DSPRes C = DSPRes::zero();
+
+    if (w1.is_negative())
+    {
+        C.set_bit(11, true);
+        C.set_bit(22, true);
+        //        C.set_bit(33, true); <- this increases the error rate again
+    }
+
+    params.C = C;
+    return params;
+}
+
+/**
+ * @brief Helper function the extracts the individual results from the DSP's output bitstring
+ * @param result The result computed by the DSP
+ * @return The individual extracted results
+ */
+[[nodiscard]] std::array<aint8, 4> extract_results(const DSPRes result)
+{
+    const aint8 a1w1{bit_range<7, 0>(result)};
+    const aint8 a2w1{bit_range<18, 11>(result)};
+    const aint8 a1w2{bit_range<29, 22>(result)};
+    const aint8 a2w2{bit_range<40, 33>(result)};
 
     return {a2w2, a1w2, a2w1, a1w1};
 }
 
-int test_single_packing(const aint4& w1, const aint4& w2, const auint4& a1, const auint4& a2)
+template <typename Packing>
+[[nodiscard]] std::array<aint8, 4> generic_approach(const aint4& w1, const aint4& w2,
+                                                    const auint4& a1, const auint4& a2,
+                                                    Packing packing)
+{
+    const DSPIn params = packing(w1, w2, a1, a2);
+
+    const DSPRes dsp_res = dsp(params);
+
+    return extract_results(dsp_res);
+}
+
+template <typename Packing>
+int test_single_packing(const aint4& w1, const aint4& w2, const auint4& a1, const auint4& a2,
+                        Packing packing, const bool show_intermediate_results = false)
 {
 
     aint8 w1_{w1};
@@ -63,23 +153,31 @@ int test_single_packing(const aint4& w1, const aint4& w2, const auint4& a1, cons
 
     const std::array<aint8, 4> res_correct = {a2_ * w2_, a1_ * w2_, a2_ * w1_, a1_ * w1_};
 
-    const std::array<aint8, 4> res_dsp = pack_dsp_xilinx(w1, w2, a1, a2);
+    const std::array<aint8, 4> res_dsp = generic_approach(w1, w2, a1, a2, packing);
 
     bool error = false;
 
     for (size_t i = 0; i < 4; ++i)
     {
         error |= res_correct[i] != res_dsp[i];
-        std::cout << res_correct[i] << ";" << res_dsp[i] << ";" << (res_correct[i] - res_dsp[i])
-                  << ";";
+        if (show_intermediate_results)
+        {
+
+            std::cout << res_correct[i] << ";" << res_dsp[i] << ";" << (res_correct[i] - res_dsp[i])
+                      << ";";
+        }
     }
 
-    std::cout << "\n";
+    if (show_intermediate_results)
+    {
+        std::cout << "\n";
+    }
 
     return error;
 }
 
-void test_packing()
+template <typename Packing>
+void test_packing(Packing packing, const bool show_intermediate_results = false)
 {
     size_t n_errors = 0;
     size_t n_tests = 0;
@@ -91,25 +189,19 @@ void test_packing()
             {
                 for (auint4 a2 : integer_range<auint4>())
                 {
-                    n_errors += test_single_packing(w1, w2, a1, a2);
+                    n_errors +=
+                        test_single_packing(w1, w2, a1, a2, packing, show_intermediate_results);
                     ++n_tests;
                 }
             }
         }
     }
-    std::cout << n_errors << ";" << n_tests << "\n";
+    std::cout << n_errors << ";" << n_tests << ";" << ((float)n_errors / (float)n_tests) << "\n";
 }
 
 int main()
 {
-
-    const aint4 w1(-8);
-    const aint4 w2 = aint4::zero();
-
-    const auint4 a1 = auint4::one();
-    const auint4 a2 = auint4::one();
-
-    test_single_packing(w1, w2, a1, a2);
-    test_packing();
+    test_packing(pack_xilinx);
+    test_packing(pack_akif);
     return 0;
 }
