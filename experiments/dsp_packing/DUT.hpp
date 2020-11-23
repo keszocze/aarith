3cc94a9e76d7176fd08496abfaec552227aef0d8
Oliver Keszöcze <oliver.keszoecze@fau.de>
2020-11-23 13:23:43 +0100

    working for the simple better packing

--- experiments/dsp_packing/DUT.hpp
+++ experiments/dsp_packing/DUT.hpp
@@ -5,149 +5,177 @@
 
 namespace dsp_packing {
 
-template <typename Packing, typename Postprocessing, size_t WWidth = 4, size_t AWidth = 4>
-PPort DUT(const aarith::integer<WWidth>& w1, const aarith::integer<WWidth>& w2,
-          const aarith::uinteger<AWidth> a1, const aarith::uinteger<AWidth> a2, Packing pack,
-          Postprocessing post_proc)
+template <typename Packing, size_t WWidth = 4, size_t AWidth = 4> class DUT
 {
+public:
+    using ret_val = aarith::integer<AWidth + WWidth>;
+    using w_val = aarith::integer<WWidth>;
+    using a_val = aarith::uinteger<AWidth>;
 
-    const DSPInput in = pack(w1, w2, a1, a2);
+private:
+    Packing packing;
+    std::function<std::array<ret_val, 4>(
+        const w_val&, const w_val&,
+        const a_val&, const a_val&, const ret_val&,
+        const ret_val&, const ret_val&, const ret_val&)>
+        post_proc;
 
-    const PPort dsp_result = dsp(in);
+public:
+    static constexpr size_t W = WWidth;
+    static constexpr size_t A = AWidth;
 
-    return post_proc(dsp_result);
+    DUT(Packing packing, std::function<PPort(PPort)> post_proc)
+        : packing(packing)
+        , post_proc(post_proc)
+    {
+    }
+
+    DUT(Packing packing)
+        : packing(packing)
+        , post_proc([](const w_val& w1, const w_val& w2,
+                       const a_val& a1, const a_val& a2,
+                       const ret_val& a2w2, const ret_val& a1w2, const ret_val& a2w1,
+                       const ret_val& a1w1) {
+            return std::array<ret_val, 4>{a2w2, a1w2, a2w1, a1w1};
+        })
+    {
+    }
+
+    /**
+     * @brief Helper function the extracts the individual results from the DSP's output bitstring
+     * @param result The result computed by the DSP
+     * @return The individual extracted results
+     */
+    [[nodiscard]] std::array<ret_val, 4> extract_results(const PPort result)
+    {
+
+        static_assert(WWidth + AWidth < 12,
+                      "Using more than 11 bits (combined) for the weights and "
+                      "activation values would lead to overlapping values");
+
+        using E = ret_val;
+
+        static constexpr size_t n_guards = 11 - (WWidth + AWidth);
+        static constexpr size_t offset = (WWidth + AWidth) - 1;
+
+        const E a1w1{aarith::bit_range<offset, 0>(result)};
+        const E a2w1{aarith::bit_range<11 + offset, 11>(result)};
+        const E a1w2{aarith::bit_range<22 + offset, 22>(result)};
+        const E a2w2{aarith::bit_range<33 + offset, 33>(result)};
+
+        return {a2w2, a1w2, a2w1, a1w1};
+    }
+
+    std::array<aarith::integer<WWidth + AWidth>, 4> operator()(const w_val& w1,
+                                                               const w_val& w2,
+                                                               const a_val& a1,
+                                                               const a_val& a2)
+    {
+
+        const DSPInput in = packing(w1, w2, a1, a2);
+
+        const PPort dsp_result = dsp(in);
+
+        const auto [a2w2_, a1w2_, a2w1_, a1w1_] = extract_results(dsp_result);
+
+        return post_proc(w1, w2, a1, a2, a2w2_, a1w2_, a2w1_, a1w1_);
+    }
 };
 
-template <typename Packing, size_t WWidth = 4, size_t AWidth = 4>
-PPort DUT(const aarith::integer<WWidth>& w1, const aarith::integer<WWidth>& w2,
-          const aarith::uinteger<AWidth> a1, const aarith::uinteger<AWidth> a2, Packing pack)
+template <typename Design> class DefaultEvaluator
 {
-    return DUT(w1, w2, a1, a2, pack, [](const PPort p) { return p; });
+    using val = aarith::integer<Design::A + Design::W>;
+    using val_w = aarith::integer<Design::W>;
+    using val_a = aarith::uinteger<Design::A>;
+    size_t n_errors = 0;
+    size_t n_individual_errors = 0;
+    size_t n_tests = 0;
+    val max_error{0};
+    bool print_individual_results = false;
+
+    Design design;
+
+public:
+    void setup()
+    {
+        // does nothing right now but we might come up with something interesting
+    }
+    void teardown()
+    {
+        std::cout << n_errors << ";" << n_tests << ";" << ((float)n_errors / (float)n_tests) << ";"
+                  << n_individual_errors << ";" << max_error << "\n";
+    }
+
+    DefaultEvaluator(Design design, const bool print_individual_results = false)
+        : design(design)
+        , print_individual_results(print_individual_results)
+    {
+    }
+
+    void operator()(const val_w& w1, const val_w& w2, const val_a a1, const val_a a2)
+    {
+
+        val w1_{w1};
+        val w2_{w2};
+
+        val a1_{a1};
+        val a2_{a2};
+
+        const std::array<val, 4> res_correct = {a2_ * w2_, a1_ * w2_, a2_ * w1_, a1_ * w1_};
+
+        const std::array<val, 4> res_dsp = design(w1, w2, a1, a2);
+
+        bool error = false;
+
+        for (size_t i = 0; i < 4; ++i)
+        {
+
+            if (res_correct[i] != res_dsp[i])
+            {
+                error = true;
+                ++n_individual_errors;
+                const auto diff = (res_correct[i] - res_dsp[i]);
+                //                if (diff > val{1}) {
+                //                    print_int(w1, "w1");
+                //                    print_int(w2, "w2");
+                //                    print_int(a1, "a1");
+                //                    print_int(a2, "a2");
+                //                    print_int(res_correct[i], "korrekt");
+                //                    print_int(res_dsp[i], "dsp");
+                //                }
+                max_error = max(max_error, diff);
+            }
+        }
+
+        if (print_individual_results)
+        {
+            //                    std::cout << to_binary(a1) << " " << to_binary(a2) << " " <<
+            //                    to_binary(w1) << " "
+            //                    << to_binary(w2) << " ";
+            std::cout << to_binary(res_dsp[3]) << " " << to_binary(res_dsp[2]) << " "
+                      << to_binary(res_dsp[1]) << " " << to_binary(res_dsp[0]) << "\n";
+        }
+
+        if (error)
+        {
+            ++n_errors;
+        }
+        ++n_tests;
+    }
+};
+
+template <template <typename> typename Evaluator, typename Design>
+void evaluate(Design design, const bool print_output = false)
+{
+    Evaluator<Design> e{design, print_output};
+    e.setup();
+    iterate_inputs(e);
+    e.teardown();
 }
 
-/**
- * @brief Helper function the extracts the individual results from the DSP's output bitstring
- * @param result The result computed by the DSP
- * @return The individual extracted results
- */
-template <size_t WWidth = 4, size_t AWidth = 4>
-[[nodiscard]] std::array<aarith::integer<WWidth + AWidth>, 4> extract_results(const PPort result)
+template <typename D> void evaluate(D design, const bool print_output = false)
 {
-
-    static_assert(WWidth + AWidth < 12, "Using more than 11 bits (combined) for the weights and "
-                                        "activation values would lead to overlapping values");
-
-    using E = aarith::integer<WWidth + AWidth>;
-
-    static constexpr size_t n_guards = 11 - (WWidth + AWidth);
-    static constexpr size_t offset = (WWidth + AWidth) - 1;
-
-    const E a1w1{aarith::bit_range<offset, 0>(result)};
-    const E a2w1{aarith::bit_range<11 + offset, 11>(result)};
-    const E a1w2{aarith::bit_range<22 + offset, 22>(result)};
-    const E a2w2{aarith::bit_range<33 + offset, 33>(result)};
-
-    return {a2w2, a1w2, a2w1, a1w1};
+    evaluate<DefaultEvaluator, D>(design, print_output);
 }
-
-/**
- * @brief Generic function for DSP packing for the use of multiple multiplications in a single DSP.
- *
- * The function packs the weights and activation variables using the provided packing function.
- * Afterwards, the data is fed to the DSP. The result is then extracted and returned.
- *
- * @tparam Packing Function template parameter for the packing to use
- * @param w1 Weight 1
- * @param w2 Weight 2
- * @param a1 Activation value 1
- * @param a2 Activation value 2
- * @param packing The concrete packing that is used to feed the DSP
- * @return Individual results
- */
-template <typename Packing, typename PostProcessing, size_t WWidth = 4, size_t AWidth = 4>
-[[nodiscard]] std::array<aarith::integer<WWidth + AWidth>, 4>
-call_dut(const aarith::integer<WWidth>& w1, const aarith::integer<WWidth>& w2,
-                 const aarith::uinteger<AWidth>& a1, const aarith::uinteger<AWidth>& a2,
-                 Packing packing, PostProcessing post_proc)
-{
-    return extract_results(DUT(w1,w2,a1,a2,packing,post_proc));
-}
-
-/**
- * @brief Generic function for DSP packing for the use of multiple multiplications in a single DSP.
- *
- * The function packs the weights and activation variables using the provided packing function.
- * Afterwards, the data is fed to the DSP. The result is then extracted and returned.
- *
- * @tparam Packing Function template parameter for the packing to use
- * @param w1 Weight 1
- * @param w2 Weight 2
- * @param a1 Activation value 1
- * @param a2 Activation value 2
- * @param packing The concrete packing that is used to feed the DSP
- * @return Individual results
- */
-template <typename Packing, size_t WWidth = 4, size_t AWidth = 4>
-[[nodiscard]] std::array<aarith::integer<WWidth + AWidth>, 4>
-call_dut(const aarith::integer<WWidth>& w1, const aarith::integer<WWidth>& w2,
-                 const aarith::uinteger<AWidth>& a1, const aarith::uinteger<AWidth>& a2,
-                 Packing packing)
-{
-    return extract_results(DUT(w1,w2,a1,a2,packing));
-}
-
-
-
-//template <typename Packing> class DefaultEvaluator
-//{
-//    size_t n_errors = 0;
-//    size_t n_individual_errors = 0;
-//    size_t n_tests = 0;
-//    aint8 max_error{0};
-//    Packing packing;
-//    bool print_individual_result = false;
-//
-//public:
-//    void setup()
-//    {
-//        // does nothing right now but we might come up with something interesting
-//    }
-//    void teardown()
-//    {
-//        std::cout << n_errors << ";" << n_tests << ";" << ((float)n_errors / (float)n_tests) << ";"
-//                  << n_individual_errors << ";" << max_error << "\n";
-//    }
-//
-//    D(Packing packing, const bool print_individual_results = false)
-//        : packing(packing)
-//        , print_individual_result(print_individual_results)
-//    {
-//    }
-//
-//    void operator()(const aint4& w1, const aint4& w2, const auint4 a1, const auint4 a2)
-//    {
-//        const auto res = test_single_packing(w1, w2, a1, a2, packing, print_individual_result);
-//        n_errors += std::get<0>(res);
-//        n_individual_errors += std::get<1>(res);
-//        max_error = max(max_error, std::get<2>(res));
-//        ++n_tests;
-//    }
-//};
-//
-//
-//
-//template <typename D, template<size_t, size_t> typename Evaluator, size_t WWidth = 4, size_t AWidth = 4>
-//void evaluate(D design, const bool print_output=false) {
-//    Evaluator<WWidth, AWidth> e{design, print_output};
-//    e.setup();
-//    iterate_inputs(e);
-//    e.teardown();
-//}
-//
-//
-//template <typename D, size_t WWidth = 4, size_t AWidth = 4>
-//void evaluate(D design, const bool print_output=false) {
-//    evaluate<D,DefaultEvaluator, WWidth, AWidth>(design, print_output);
-//}
 
 } // namespace dsp_packing
--- experiments/dsp_packing/dsp_packing.cpp
+++ experiments/dsp_packing/dsp_packing.cpp
@@ -62,140 +62,21 @@ DSPInput pack_akif(const aint4& w1, const aint4& w2, const auint4& a1, const aui
     return params;
 }
 
-
-
-/**
- * @brief Helper function for debugging.
- *
- * It is unfinished because I found one error that Akif pointed out already.
- *
- * @tparam Packing
- * @param w1
- * @param w2
- * @param a1
- * @param a2
- * @param packing
- */
-template <typename Packing>
-void generic_investigate(const aint4& w1, const aint4& w2, const auint4& a1, const auint4& a2,
-                         Packing packing)
-{
-
-    print_int(w1, "w1");
-    print_int(w2, "w2");
-    print_int(a1, "a1");
-    print_int(a2, "a2");
-
-    const DSPInput params = packing(w1, w2, a1, a2);
-
-    print_int(params.A, "A");
-    print_int(params.D, "D");
-    print_int(params.B, "B");
-    print_int(params.C, "C");
-
-    const PPort dsp_res = dsp(params);
-
-    print_int(dsp_res, "P");
-
-    aint8 w1_{w1};
-    aint8 w2_{w2};
-
-    aint8 a1_{a1};
-    aint8 a2_{a2};
-
-    const std::array<aint8, 4> res_correct = {a2_ * w2_, a1_ * w2_, a2_ * w1_, a1_ * w1_};
-    const std::array<aint8, 4> res_dsp = extract_results(dsp_res);
-
-    print_int(res_correct[0], "a2w2 correct");
-    print_int(res_correct[1], "a1w2 correct");
-    print_int(res_correct[2], "a2w1 correct");
-    print_int(res_correct[3], "a1w1 correct");
-
-    print_int(res_dsp[0], "a2w2 dsp");
-    print_int(res_dsp[1], "a1w2 dsp");
-    print_int(res_dsp[2], "a2w1 dsp");
-    print_int(res_dsp[3], "a1w1 dsp");
-}
-
-/**
- * @brief Evaluates a single packed input
- * @tparam Packing
- * @param w1 Weight 1
- * @param w2 Weight 2
- * @param a1 Activation value 1
- * @param a2 Activation value 2
- * @param packing The concrete packing that is used to feed the DSP
- * @param show_intermediate_results If true, all computations are printed to standard out
- * @return (1 iff an error occured, #individual errors, max error in that line)
- */
-template <typename Packing>
-std::tuple<size_t, size_t, aint8>
-test_single_packing(const aint4& w1, const aint4& w2, const auint4& a1, const auint4& a2,
-                    Packing packing, const bool show_intermediate_results = false)
-{
-
-    aint8 w1_{w1};
-    aint8 w2_{w2};
-
-    aint8 a1_{a1};
-    aint8 a2_{a2};
-
-    const std::array<aint8, 4> res_correct = {a2_ * w2_, a1_ * w2_, a2_ * w1_, a1_ * w1_};
-
-    const std::array<aint8, 4> res_dsp = call_dut(w1, w2, a1, a2, packing);
-
-    bool error = false;
-    int num_error = 0;
-    aint8 max_error{0};
-
-    for (size_t i = 0; i < 4; ++i)
-    {
-        error |= res_correct[i] != res_dsp[i];
-        num_error += res_correct[i] != res_dsp[i];
-        const auto diff = (res_correct[i] - res_dsp[i]);
-
-        max_error = max(max_error, diff);
-    }
-
-    if (show_intermediate_results)
-    {
-        //        std::cout << to_binary(a1) << " " << to_binary(a2) << " " << to_binary(w1) << " "
-        //        << to_binary(w2) << " ";
-        std::cout << to_binary(res_dsp[3]) << " " << to_binary(res_dsp[2]) << " "
-                  << to_binary(res_dsp[1]) << " " << to_binary(res_dsp[0]) << "\n";
-    }
-
-    return {error, num_error, max_error};
-}
-
-
-
-//template <typename Packing>
-//void test_packing(Packing packing, const bool print_individual_results = false)
-//{
-//    D t{packing, print_individual_results};
-//    t.setup();
-//    iterate_inputs(t);
-//    t.teardown();
-//}
+// TODO Implement other simple addition correction
+// TODO Implement Akif's correction circuit
 
 } // namespace dsp_packing
+
 
 int main()
 {
 
     using namespace dsp_packing;
 
-    //    aint4 w1{0};
-    //    aint4 w2{1};
-    //    auint4 a1{0};
-    //    auint4 a2{8};
-    //        generic_investigate(w1, w2, a1, a2, pack_xilinx);
+    DUT design_under_test(pack_akif);
 
-//    test_packing(pack_xilinx, false);
-//    test_packing(pack_akif, true);
 
-    print_inputs();
+    evaluate(design_under_test, true);
 
     return 0;
 }
