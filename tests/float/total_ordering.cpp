2c04c9b06a34442f9c03ce8469c1096ec0961972
Oliver Keszöcze <oliver.keszoecze@fau.de>
2020-11-24 19:03:39 +0100

    Add test cases for self total ordering

--- tests/float/total_ordering.cpp
+++ tests/float/total_ordering.cpp
@@ -4,6 +4,52 @@
 #include <catch.hpp>
 using namespace aarith;
 
+
+
+TEMPLATE_TEST_CASE_SIG("Total ordering +/- zero",
+                       "[normalized_float][comparison][utility]",
+                       ((size_t E, size_t M, typename Native), E, M, Native), (8, 23, float),
+                       (11, 52, double))
+{
+    using F = normalized_float<E, M>;
+
+    GIVEN("+/- zero") {
+
+        constexpr F pos_zero{F::zero()};
+        constexpr F neg_zero{F::neg_zero()};
+
+        THEN("The cordering behaves as defined in the standard") {
+            constexpr bool v = totalOrder(neg_zero,pos_zero);
+            REQUIRE(v);
+        }
+    }
+}
+
+TEMPLATE_TEST_CASE_SIG("Total ordering with itself",
+                       "[normalized_float][comparison][utility]",
+                       ((size_t E, size_t M, typename Native), E, M, Native), (8, 23, float),
+                       (11, 52, double))
+{
+    using F = normalized_float<E, M>;
+
+    GIVEN("A random, non NaN, non infinity floating-point number x")
+    {
+        F x = GENERATE(take(100, random_float<E, M, FloatGenerationModes::FullyRandom>()));
+
+        if (!x.is_nan()) {
+            WHEN("ordering the number with itself") {
+                THEN("The result should be true") {
+                    const bool b = totalOrder(x,x);
+                    if (!b) {
+                        std::cout << x << "\t" << to_binary(x) << "\n";
+                    }
+                    REQUIRE(totalOrder(x,x));
+                }
+            }
+        }
+    }
+}
+
 TEMPLATE_TEST_CASE_SIG("Total ordering with a single NaN value",
                        "[normalized_float][comparison][utility]",
                        ((size_t E, size_t M, typename Native), E, M, Native), (8, 23, float),
@@ -13,8 +59,8 @@ TEMPLATE_TEST_CASE_SIG("Total ordering with a single NaN value",
     constexpr F pqNaN = F::qNaN();
     constexpr F psNaN = F::sNaN();
 
-    F nqNaN = negate(F::qNaN());
-    F nsNaN = negate(F::sNaN());
+    constexpr F nqNaN = negate(F::qNaN());
+    constexpr F nsNaN = negate(F::sNaN());
 
     GIVEN("A random, non NaN, non infinity floating-point number x")
     {
@@ -22,60 +68,112 @@ TEMPLATE_TEST_CASE_SIG("Total ordering with a single NaN value",
 
         WHEN("totally odering the number x with NaN")
         {
-            AND_WHEN("the NaN is 'positive'") {
-                THEN("x < NaN") {
+            AND_WHEN("the NaN is 'positive'")
+            {
+                THEN("x < NaN")
+                {
                     CHECK(totalOrder(x, pqNaN));
-                    REQUIRE(totalOrder(x,psNaN));
+                    REQUIRE(totalOrder(x, psNaN));
                 }
-                THEN("!(NaN < x)") {
-                    CHECK_FALSE(totalOrder(pqNaN,x));
-                    REQUIRE_FALSE(totalOrder(psNaN,x));
+                THEN("!(NaN < x)")
+                {
+                    CHECK_FALSE(totalOrder(pqNaN, x));
+                    REQUIRE_FALSE(totalOrder(psNaN, x));
                 }
             }
-            AND_WHEN("the NaN is 'negative'") {
-                THEN("NaN < x") {
+            AND_WHEN("the NaN is 'negative'")
+            {
+                THEN("NaN < x")
+                {
                     CHECK(totalOrder(nqNaN, x));
-                    REQUIRE(totalOrder(nsNaN,x));
+                    REQUIRE(totalOrder(nsNaN, x));
                 }
-                THEN("!(x < NaN)") {
+                THEN("!(x < NaN)")
+                {
                     CHECK_FALSE(totalOrder(x, nqNaN));
-                    REQUIRE_FALSE(totalOrder(x,nsNaN));
+                    REQUIRE_FALSE(totalOrder(x, nsNaN));
                 }
             }
         }
     }
-    GIVEN("+/- infinity") {
-        constexpr F pos_inf= F::pos_infinity();
-        constexpr F neg_inf= F::neg_infinity();
+    GIVEN("+/- infinity")
+    {
+        constexpr F pos_inf = F::pos_infinity();
+        constexpr F neg_inf = F::neg_infinity();
         WHEN("totally odering the infinities with NaN")
         {
-            AND_WHEN("the NaN is 'positive'") {
-                THEN("+/- inf < NaN") {
+            AND_WHEN("the NaN is 'positive'")
+            {
+                THEN("+/- inf < NaN")
+                {
                     CHECK(totalOrder(pos_inf, pqNaN));
-                    REQUIRE(totalOrder(pos_inf,psNaN));
+                    REQUIRE(totalOrder(pos_inf, psNaN));
                     CHECK(totalOrder(neg_inf, pqNaN));
-                    REQUIRE(totalOrder(neg_inf,psNaN));
+                    REQUIRE(totalOrder(neg_inf, psNaN));
                 }
-                THEN("!(NaN < +/- inf)") {
-                    CHECK_FALSE(totalOrder(pqNaN,pos_inf));
-                    REQUIRE_FALSE(totalOrder(psNaN,pos_inf));
-                    CHECK_FALSE(totalOrder(pqNaN,neg_inf));
-                    REQUIRE_FALSE(totalOrder(psNaN,neg_inf));
+                THEN("!(NaN < +/- inf)")
+                {
+                    CHECK_FALSE(totalOrder(pqNaN, pos_inf));
+                    REQUIRE_FALSE(totalOrder(psNaN, pos_inf));
+                    CHECK_FALSE(totalOrder(pqNaN, neg_inf));
+                    REQUIRE_FALSE(totalOrder(psNaN, neg_inf));
                 }
             }
-            AND_WHEN("the NaN is 'negative'") {
-                THEN("NaN < +/- inf") {
+            AND_WHEN("the NaN is 'negative'")
+            {
+                THEN("NaN < +/- inf")
+                {
                     CHECK(totalOrder(nqNaN, pos_inf));
-                    REQUIRE(totalOrder(nsNaN,pos_inf));
+                    REQUIRE(totalOrder(nsNaN, pos_inf));
                     CHECK(totalOrder(nqNaN, neg_inf));
-                    REQUIRE(totalOrder(nsNaN,neg_inf));
+                    REQUIRE(totalOrder(nsNaN, neg_inf));
                 }
-                THEN("!(+/- inf < NaN)") {
+                THEN("!(+/- inf < NaN)")
+                {
                     CHECK_FALSE(totalOrder(pos_inf, nqNaN));
-                    REQUIRE_FALSE(totalOrder(pos_inf,nsNaN));
+                    REQUIRE_FALSE(totalOrder(pos_inf, nsNaN));
                     CHECK_FALSE(totalOrder(neg_inf, nqNaN));
-                    REQUIRE_FALSE(totalOrder(neg_inf,nsNaN));
+                    REQUIRE_FALSE(totalOrder(neg_inf, nsNaN));
                 }
+            }
+        }
+    }
+}
+
+TEMPLATE_TEST_CASE_SIG("Total ordering with a two NaN values",
+                       "[normalized_float][comparison][utility]",
+                       ((size_t E, size_t M, typename Native), E, M, Native), (8, 23, float),
+                       (11, 52, double))
+{
+    using F = normalized_float<E, M>;
+    constexpr F pqNaN = F::qNaN();
+    constexpr F psNaN = F::sNaN();
+
+    constexpr F nqNaN = negate(F::qNaN());
+    constexpr F nsNaN = negate(F::sNaN());
+
+    GIVEN("All four types of NaNs (ignoring any payloads)")
+    {
+        WHEN("Totally ordering them")
+        {
+            THEN("i) negative sign orders below positive sign")
+            {
+                CHECK(totalOrder(nqNaN, pqNaN));
+                CHECK(totalOrder(nqNaN, psNaN));
+                CHECK(totalOrder(nsNaN, psNaN));
+                CHECK(totalOrder(nsNaN, pqNaN));
+                CHECK_FALSE(totalOrder(pqNaN, nqNaN));
+                CHECK_FALSE(totalOrder(pqNaN, nsNaN));
+                CHECK_FALSE(totalOrder(psNaN, nsNaN));
+                CHECK_FALSE(totalOrder(psNaN, nqNaN));
+            }
+            THEN("ii) signaling orders below quiet for +NaN, reverse for −NaN")
+            {
+                CHECK(totalOrder(psNaN, pqNaN));
+                CHECK_FALSE(totalOrder(pqNaN, psNaN));
+
+                CHECK(totalOrder(nqNaN, nsNaN));
+                CHECK_FALSE(totalOrder(nsNaN, nqNaN));
             }
         }
     }
