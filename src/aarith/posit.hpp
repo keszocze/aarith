ee4565a937b643ff1419d210b7e2234010213f4c
Andreas Schärtl <andreas.schaertl@fau.de>
2020-12-03 16:53:01 +0100

    Add posit class

---
+++ src/aarith/posit.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <aarith/posit_no_operators.hpp>
+
+namespace aarith {
+} // namespace aarith
---
+++ src/aarith/posit_no_operators.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <aarith/core.hpp>
+
+#include <aarith/posit/posits.hpp>
