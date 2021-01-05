3d699e692f0102f9c60806378f725f1ff38ea3c8
Andreas Schärtl <andreas.schaertl@fau.de>
2021-01-05 10:38:37 +0100

    Set up testcases for posit division.

--- tests/posit/posit-softposit-testcases.hpp
+++ tests/posit/posit-softposit-testcases.hpp
@@ -2,23 +2,28 @@
 
 #include <aarith/posit.hpp>
 #include <cstdint>
+#include <functional>
+
+using namespace aarith;
 
 template <size_t N, size_t ES>
-static void require_addition(const uint32_t lhs, const uint32_t rhs, const uint32_t expected)
-{
-    using namespace aarith;
+using Op = std::function<posit<N, ES>(const posit<N, ES>&, const posit<N, ES>&)>;
 
+template <size_t N, size_t ES>
+static void require_arithmetic(const uint32_t lhs, const uint32_t rhs, const uint32_t expected,
+                               const Op<N, ES>& operation)
+{
     const posit<N, ES> p = posit<N, ES>::from(lhs);
     const posit<N, ES> q = posit<N, ES>::from(rhs);
 
-    const posit<N, ES> actual_posit = p + q;
+    const posit<N, ES> actual_posit = operation(p, q);
     const posit<N, ES> expected_posit = posit<N, ES>::from(expected);
 
     {
         INFO("  " << dump_string(p));
-        INFO("+ " << dump_string(q));
+        INFO("& " << dump_string(q));
         INFO("  " << double(p));
-        INFO("+ " << double(q));
+        INFO("& " << double(q));
         INFO("? " << dump_string(actual_posit));
         INFO("! " << dump_string(expected_posit));
         INFO("? " << double(actual_posit));
@@ -26,59 +31,32 @@ static void require_addition(const uint32_t lhs, const uint32_t rhs, const uint3
 
         REQUIRE(actual_posit == expected_posit);
     }
+}
+
+template <size_t N, size_t ES>
+static void require_addition(const uint32_t lhs, const uint32_t rhs, const uint32_t expected)
+{
+    static const Op<N, ES> operation = [](auto x, auto y) { return x + y; };
+    require_arithmetic(lhs, rhs, expected, operation);
 }
 
 template <size_t N, size_t ES>
 static void require_subtraction(const uint32_t lhs, const uint32_t rhs, const uint32_t expected)
 {
-    using namespace aarith;
-
-    const posit<N, ES> p = posit<N, ES>::from(lhs);
-    const posit<N, ES> q = posit<N, ES>::from(rhs);
-
-    const posit<N, ES> actual_posit = p - q;
-    const posit<N, ES> expected_posit = posit<N, ES>::from(expected);
-
-    {
-        INFO("  " << dump_string(p));
-        INFO("- " << dump_string(q));
-        INFO("  " << double(p));
-        INFO("- " << double(q));
-        INFO("? " << dump_string(actual_posit));
-        INFO("! " << dump_string(expected_posit));
-        INFO("? " << double(actual_posit));
-        INFO("! " << double(expected_posit));
-
-        REQUIRE(actual_posit == expected_posit);
-    }
+    static const Op<N, ES> operation = [](auto x, auto y) { return x - y; };
+    require_arithmetic(lhs, rhs, expected, operation);
 }
 
 template <size_t N, size_t ES>
 static void require_multiplication(const uint32_t lhs, const uint32_t rhs, const uint32_t expected)
 {
-    using namespace aarith;
-    std::cout << std::endl;
+    static const Op<N, ES> operation = [](auto x, auto y) { return x * y; };
+    require_arithmetic(lhs, rhs, expected, operation);
+}
 
-
-    const posit<N, ES> p = posit<N, ES>::from(lhs);
-    const posit<N, ES> q = posit<N, ES>::from(rhs);
-
-    const posit<N, ES> actual_posit = p * q;
-    const posit<N, ES> expected_posit = posit<N, ES>::from(expected);
-
-    static int i;
-
-    {
-        INFO("i " << i++);
-        INFO("  " << dump_string(p));
-        INFO("× " << dump_string(q));
-        INFO("  " << double(p));
-        INFO("× " << double(q));
-        INFO("? " << dump_string(actual_posit));
-        INFO("! " << dump_string(expected_posit));
-        INFO("? " << double(actual_posit));
-        INFO("! " << double(expected_posit));
-
-        REQUIRE(actual_posit == expected_posit);
-    }
+template <size_t N, size_t ES>
+static void require_division(const uint32_t lhs, const uint32_t rhs, const uint32_t expected)
+{
+    static const Op<N, ES> operation = [](auto x, auto y) { return x / y; };
+    require_arithmetic(lhs, rhs, expected, operation);
 }
