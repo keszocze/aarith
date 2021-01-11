23b1d4271f794c53b29dbeeda68bad5ac1d47214
Andreas Schärtl <andreas.schaertl@fau.de>
2021-01-11 19:05:09 +0100

    Print posits by converting to quire

--- src/aarith/posit/posit_operators.hpp
+++ src/aarith/posit/posit_operators.hpp
@@ -8,10 +8,7 @@ namespace aarith {
 template <size_t N, size_t ES, typename WT>
 std::ostream& operator<<(std::ostream& os, const posit<N, ES, WT>& p)
 {
-    // TODO(Schärtl): actually print decimal representation; to do this
-    // however we need to have arithmetic operators implemented first
-
-    return os << to_binary(p.get_bits());
+    return os << quire(p);
 }
 
 } // namespace aarith
