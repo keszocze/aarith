aa7e781a606b4832e477f603af214cbfe1b581a9
Andreas Schärtl <andreas.schaertl@fau.de>
2021-01-20 11:45:38 +0100

    Print valids as proper intervals

--- src/aarith/posit/valid_operators.hpp
+++ src/aarith/posit/valid_operators.hpp
@@ -7,7 +7,52 @@ namespace aarith {
 template <size_t N, size_t ES, typename WT>
 std::ostream& operator<<(std::ostream& os, const valid<N, ES, WT>& v)
 {
-    return os << "start=" << v.get_start() << " end=" << v.get_end();
+    const auto& start = v.get_start();
+    const auto& end = v.get_end();
+
+    if (start.is_uncertain())
+    {
+        if (start == start.min())
+        {
+            os << "(-∞";
+        }
+        else if (start == start.max())
+        {
+            os << "(+∞";
+        }
+        else
+        {
+            os << "(" << start.value();
+        }
+    }
+    else
+    {
+        os << "[" << start.value();
+    }
+
+    os << ", ";
+
+    if (end.is_uncertain())
+    {
+        if (end == end.min())
+        {
+            os << "-∞)";
+        }
+        else if (end == end.max())
+        {
+            os << "+∞)";
+        }
+        else
+        {
+            os << end.value() << ")";
+        }
+    }
+    else
+    {
+        os << end.value() << "]";
+    }
+
+    return os;
 }
 
 } // namespace aarith
