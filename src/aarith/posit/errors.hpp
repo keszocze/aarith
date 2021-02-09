9170621b77b466e38c50391b325ea02c7a9e429c
Andreas Schärtl <andreas.schaertl@fau.de>
2021-02-09 10:12:51 +0100

    Declare posit errors as inline
    Without the inline declaration we are getting "multiple definitions"
    errors when using the library. I fear there are probably more cases like
    this in the posit tree.

--- src/aarith/posit/errors.hpp
+++ src/aarith/posit/errors.hpp
@@ -4,12 +4,12 @@
 
 namespace aarith {
 
-posit_error::posit_error(const std::string& whatarg)
+inline posit_error::posit_error(const std::string& whatarg)
     : std::runtime_error(whatarg)
 {
 }
 
-nar_error::nar_error(const std::string& whatarg)
+inline nar_error::nar_error(const std::string& whatarg)
     : posit_error(whatarg)
 {
 }
