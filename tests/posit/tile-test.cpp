3b4dcf0f6ade1e15d58f69a13851706dea5170c6
Andreas Schärtl <andreas.schaertl@fau.de>
2021-02-23 10:16:27 +0100

    tests: add tests for tile endpoints

---
+++ tests/posit/tile-test.cpp
@@ -0,0 +1,27 @@
+#include <catch.hpp>
+
+#include <aarith/posit.hpp>
+
+#include "../test-signature-ranges.hpp"
+#include "for_each_posit.hpp"
+#include "gen_posit.hpp"
+
+TEMPLATE_TEST_CASE_SIG("getting endpoints from tiles", "[posit][][template]",
+                       ((size_t N, size_t ES), N, ES), AARITH_POSIT_TEST_TEMPLATE_EXHAUSTABLE)
+{
+    using namespace aarith;
+
+    using Posit = posit<N, ES>;
+    using Tile = tile<N, ES>;
+
+    for_each_posit<Posit>([](const Posit& p) {
+        const Tile certain = Tile::from(p, false);
+        const Tile uncertain = Tile::from(p, true);
+
+        REQUIRE(certain.as_start_value() == p);
+        REQUIRE(uncertain.as_start_value() == p);
+
+        REQUIRE(certain.as_end_value() == p);
+        REQUIRE(uncertain.as_end_value() == p.incremented());
+    });
+}
