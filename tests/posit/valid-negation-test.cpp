cf7d9898c3109ee895033130edca0dde7ecc37d8
Andreas Schärtl <andreas.schaertl@fau.de>
2021-03-02 11:32:19 +0100

    Add tests for unary minus on valids

---
+++ tests/posit/valid-negation-test.cpp
@@ -0,0 +1,79 @@
+#include <catch.hpp>
+
+#include <aarith/posit.hpp>
+
+SCENARIO("valid negation")
+{
+    using namespace aarith;
+
+    using Posit = posit<3, 1>;
+    using Valid = valid<3, 1>;
+
+    GIVEN("arbitrary valids")
+    {
+        const Posit four = Posit(4.0);
+        const Posit one = Posit(1.0);
+        const Posit fourth = Posit(0.25);
+        const Posit zero = Posit(0.0);
+
+        const Posit nfour = Posit(-4.0);
+        const Posit none = Posit(-1.0);
+        const Posit nfourth = Posit(-0.25);
+
+        constexpr bool open = true;
+        constexpr bool closed = false;
+
+        THEN("assert that negation works as expected")
+        {
+            {
+                const Valid v = Valid::from(none, closed, one, closed); // [-1, 1]
+                const Valid v_negated = -v;
+                REQUIRE(v_negated.in_interval_notation() == "[-1, 1]");
+            }
+
+            {
+                const Valid v = Valid::from(none, open, zero, open); // (-1, 0)
+                const Valid v_negated = -v;
+                REQUIRE(v_negated.in_interval_notation() == "(0, 1)");
+            }
+
+            {
+                const Valid v = Valid::from(nfour, closed, one, open); // [-4, 1)
+                const Valid v_negated = -v;
+                REQUIRE(v_negated.in_interval_notation() == "(-1, 4]");
+            }
+
+            {
+                const Valid v = Valid::from(fourth, open, one, closed); // (0.25, 1]
+                const Valid v_negated = -v;
+                REQUIRE(v_negated.in_interval_notation() == "[-1, -0.25)");
+            }
+
+            {
+                const Valid v = Valid(four);
+                const Valid v_negated = -v;
+                REQUIRE(v_negated.in_interval_notation() == "-4");
+            }
+
+            {
+                const Valid v = Valid(nfourth);
+                const Valid v_negated = -v;
+                REQUIRE(v_negated.in_interval_notation() == "0.25");
+            }
+        }
+    }
+
+    GIVEN("extreme values")
+    {
+        THEN("assert that negation works as expected")
+        {
+            const auto vmax = Valid::max();
+            const auto vmin = Valid::min();
+            const auto zero = Valid::zero();
+
+            REQUIRE(-vmax == vmin);
+            REQUIRE(-vmin == vmax);
+            REQUIRE(-zero == zero);
+        }
+    }
+}
