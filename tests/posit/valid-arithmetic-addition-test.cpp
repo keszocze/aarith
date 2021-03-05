037efd6e200e155d9a14d2a56f0cee94bccfea17
Andreas Schärtl <andreas.schaertl@fau.de>
2021-03-05 11:32:15 +0100

    Clean up test cases for valid addition and subtraction

--- tests/posit/valid-arithmetic-addition-test.cpp
+++ tests/posit/valid-arithmetic-addition-test.cpp
@@ -3,47 +3,7 @@
 #include <aarith/posit.hpp>
 
 #include "../test-signature-ranges.hpp"
-#include "gen_tile.hpp"
-
-TEMPLATE_TEST_CASE_SIG("addition regular case", "[valid][posit][template]",
-                       ((size_t N, size_t ES), N, ES), AARITH_POSIT_TEST_TEMPLATE_EXHAUSTABLE)
-{
-    /*
-    using namespace aarith;
-
-    using Valid = valid<N, ES>;
-    using Tile = tile<N, ES>;
-
-    // Computing {a, b} + {c, d} = {x, y}
-
-    const Tile a = GENERATE(take(10, random_tile<Tile>()));
-    const Tile b = GENERATE(take(10, random_tile<Tile>()));
-    const Tile c = GENERATE(take(10, random_tile<Tile>()));
-    const Tile d = GENERATE(take(10, random_tile<Tile>()));
-
-    const Valid v = Valid::from(a, b);
-    const Valid w = Valid::from(c, d);
-
-    const Valid sum = v + w;
-
-    // While we generate a lot of test cases, some of them require
-    // different tests. We only care about the regular cases here.
-    // Which means lots of filtering out.
-
-    if (any_irregular({a, b, c, d}) || any_irregular({v, w, sum}))
-    {
-        return;
-    }
-
-    // Finally we can run the real test case.
-
-    const Tile& x = sum.get_start();
-    const Tile& y = sum.get_end();
-
-    REQUIRE(x.value() == (a.value() + c.value()));
-    REQUIRE(y.value() == (b.value() + d.value()));
-    */
-}
+#include "gen_posit.hpp"
 
 SCENARIO("adding arbitrary posit-correct valids")
 {
@@ -319,36 +279,40 @@ TEMPLATE_TEST_CASE_SIG("adding zero does not change the result (random)",
     {
         const Valid v = Valid::from(start, open, end, open);
 
-        if (!v.is_irregular())
+        if (v.is_regular())
         {
             REQUIRE((v + zero) == v);
+            REQUIRE((zero + v) == v);
         }
     }
 
     {
         const Valid v = Valid::from(start, open, end, closed);
 
-        if (!v.is_irregular())
+        if (v.is_regular())
         {
             REQUIRE((v + zero) == v);
+            REQUIRE((zero + v) == v);
         }
     }
 
     {
         const Valid v = Valid::from(start, closed, end, open);
 
-        if (!v.is_irregular())
+        if (v.is_regular())
         {
             REQUIRE((v + zero) == v);
+            REQUIRE((zero + v) == v);
         }
     }
 
     {
         const Valid v = Valid::from(start, closed, end, closed);
 
-        if (!v.is_irregular())
+        if (v.is_regular())
         {
             REQUIRE((v + zero) == v);
+            REQUIRE((zero + v) == v);
         }
     }
 }
