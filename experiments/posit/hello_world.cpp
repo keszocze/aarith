f150802e13478406ad3d3137b821715b2a059820
Andreas Schärtl <andreas.schaertl@fau.de>
2021-04-07 14:05:31 +0200

    Add placeholder experiment for posits

---
+++ experiments/posit/hello_world.cpp
@@ -0,0 +1,23 @@
+/*
+ * Hello World
+ *
+ * Count up from -maxpos to maxpos. Not very interesting, but enough for trying
+ * out the build.
+ */
+
+#include <iostream>
+
+#include <aarith/posit.hpp>
+
+using namespace aarith;
+
+int main()
+{
+    posit8 p = posit8::min();
+
+    while (!p.is_nar())
+    {
+        std::cout << p << std::endl;
+        p = p.incremented();
+    }
+}
